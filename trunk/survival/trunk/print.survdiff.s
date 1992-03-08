076c806bcb4a7429fce2c7954fe984a076c31b7f
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-03-08 20:13:48 +0000

    Add support for 1-sample test.  Return the input arg per 'print'.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9708 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/print.survdiff.s
+++ trunk/survival/trunk/print.survdiff.s
@@ -1,4 +1,4 @@
-#SCCS $Date: 1992-03-04 16:48:21 $ $Id: print.survdiff.s,v 4.1 1992-03-04 16:48:21 therneau Exp $
+#SCCS $Date: 1992-03-08 20:13:48 $ $Id: print.survdiff.s,v 4.2 1992-03-08 20:13:48 therneau Exp $
 print.surv.diff <- function(diff.list, digits=4, ...) {
 
     fit <- diff.list
@@ -13,13 +13,25 @@ print.surv.diff <- function(diff.list, digits=4, ...) {
 	cat("\n")
 	}
 
+    omit <- attr(fit$n, 'omit')
+    if (length(omit)) cat("n=", sum(fit$n), ", ", length(omit),
+					  "deleted due to missing.\n\n")
+
     temp <- cbind(fit$n, fit$obs, fit$exp, ((fit$obs-fit$exp)^2)/ fit$exp)
-    dimnames(temp) <- list(names(fit$n), c("N", "Observed", "Expected",
+    if (length(fit$n)==1)  {
+	df <- fit$n   #one sample test
+	dimnames(temp) <- list("One sample test",
+				c("N", "Observed", "Expected",  "(O-E)^2/E"))
+	}
+    else {
+	df <- length(fit$n) -1
+	dimnames(temp) <- list(names(fit$n), c("N", "Observed", "Expected",
 					   "(O-E)^2/E"))
+       }
     print(temp)
-    df <- length(fit$n) -1
+
     cat("\n Chisq=", format(round(fit$chisq,1)),
 	     " on", df, "degrees of freedom, p=",
 	     format(signif(1-pchisq(fit$chisq, df),digits)), "\n")
-    invisible()
+    invisible(fit)
     }
