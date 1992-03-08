d6517d57562a76e40447a4761730b39b9d1b4982
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-03-08 20:16:28 +0000

    Add support for one-sample tests, using the offset statement
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9710 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survdiff.s
+++ trunk/survival/trunk/survdiff.s
@@ -1,6 +1,5 @@
-#SCCS $Date: 1992-03-08 20:15:16 $ $Id: survdiff.s,v 4.2 1992-03-08 20:15:16 therneau Exp $
-surv.diff <- function(formula, rho=0, riskwt,
-		      weights, subset) {
+#SCCS $Date: 1992-03-08 20:16:28 $ $Id: survdiff.s,v 4.3 1992-03-08 20:16:28 therneau Exp $
+surv.diff <- function(formula, rho=0, subset) {
     call <- match.call()
     m <- match.call(expand=F)
     m$... <- m$rho <- m$riskwt <- NULL
@@ -17,39 +16,71 @@ surv.diff <- function(formula, rho=0, riskwt,
     m[[1]] <- as.name("model.frame")
     m <- eval(m, sys.parent())
     Terms <- attr(m, 'terms')
-    Y <- model.extract(m, response)
-    ny <- ncol(Y)
-    n <- nrow(Y)
-    casewt <- model.extract(m, "weights")
-    if (missing(riskwt)) riskwt <- rep(1,n)
-    else if (length(riskwt) != n) stop("Wrong length for 'riskwt'")
+    omit <- attr(m, 'omit')
+    y <- model.extract(m, response)
+    ny <- ncol(y)
+    n <- nrow(y)
 
-    if (length(m) == 1) strats <- factor(rep(1,n))
-    else if (length(m)==2) strats <- factor(m[[2]])
-    else {
-	i <- seq(2, length(m))
-	temp <- paste("as.character(m[[" ,i,"]]), ", sep='', collapse="', ',")
-	strats <- factor(eval(parse(text=paste("paste(", temp, "sep='')" ))))
+    offset<- attr(Terms, "offset")
+    if (!is.null(offset)) {
+	#one sample test
+	offset <- as.numeric(m[[offset]])
+	if (length(Terms)>0) stop("Cannot have both an offset and groups")
+	if (any(offset <0 | offset >1))
+	    stop("The offset must be a survival probability")
+	expected <- sum(1-offset)
+	observed <- sum(y[,ny])
+	if (rho!=0) {
+	    num <- sum(1/rho - ((1/rho + y[,ny])*offset^rho))
+	    var <- sum(1- offset^(2*rho))/(2*rho)
+	    }
+	else {
+	    var <-  sum(-log(offset))
+	    num <-  var - observed
+	    }
+	chi <- num*num/var
 	}
-    strat2 <- as.numeric(strats)
-    ngroup <- max(strat2)
-    if (ngroup <2) stop ("There is only 1 group")
 
-    if (ny!=2) stop("Surf.diff does not apply to interval time data (yet)")
-    ord <- order(Y[,1])
+    else {
+	#k sample test
+	ll <- attr(Terms, 'term.labels')
+	if (length(ll) == 0) strats <- factor(rep(1,n))
+	else {
+	    temp <-  rep(1, length(ll))
+	    strat <- attr(Terms, 'specials')$strata
+	    if (length(strat)) temp[strat] <- 0
+	    lname <- ifelse(temp==1, paste(ll,'=', sep=''), "")
+	    temp <- paste("'",lname, "', ","as.character(m[['", ll, "']])", sep='')
+	    temp <- paste(temp, collapse=", ', ',")
+	    temp <- paste("paste(", temp, ",sep='')")
+	    strats <- factor(eval(parse(text=temp)))
+	    }
+	strat2 <- as.numeric(strats)
+	ngroup <- max(strat2)
+	if (ngroup <2) stop ("There is only 1 group")
 
-    xx <- .C("survdiff", as.integer(n), as.integer(ngroup), as.double(rho),
-		   as.double(Y[ord,1]),
-		   as.integer(Y[ord,2]),
-		   as.integer(strat2[ord]),
-		   observed = double(ngroup),
-		   expected = double(ngroup),
-		   var.e    = double((ngroup-1) * (ngroup-1)),
-		   double(ngroup), double(n))
-    temp <- table(strats)
-    temp2 <- (xx$observed - xx$expected) [-1]
-    chi  <- sum(solve(matrix(xx$var.e, ncol=ngroup-1), temp2) * temp2)
-    rval <-list(n= temp, obs = xx$observed, exp=xx$expected,
+	if (ny!=2) stop("Surf.diff does not apply to interval time data (yet)")
+	ord <- order(y[,1])
+
+	xx <- .C("survdiff", as.integer(n),
+		       as.integer(ngroup),
+		       as.double(rho),
+		       as.double(y[ord,1]),
+		       as.integer(y[ord,2]),
+		       as.integer(strat2[ord]),
+		       observed = double(ngroup),
+		       expected = double(ngroup),
+		       var.e    = double((ngroup-1) * (ngroup-1)),
+		       double(ngroup), double(n))
+	n <- table(strats)
+	expected <- xx$expected
+	observed <- xx$observed
+	temp2 <- (observed - expected) [-1]
+	chi  <- sum(solve(matrix(xx$var.e, ncol=ngroup-1), temp2) * temp2)
+	}
+
+    if (length(omit)) attr(n, 'omit') <- omit
+    rval <-list(n= n, obs = observed, exp=expected,
 		    chisq= chi)
     attr(rval, "class") <- 'surv.diff'
     rval
