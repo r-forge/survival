699f4f63873cd8daa2d8afc8a55b542784de916d
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-03-09 00:17:17 +0000

    make leap year more accurate (365.2425); add an error message for entry before birth; fix bug with 'matrix' style where surv matrix was transposed; fix bug in which the wrong year column was sometimes used.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9715 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survexp.s
+++ trunk/survival/trunk/survexp.s
@@ -1,4 +1,4 @@
-#SCCS $Date: 1992-03-08 20:19:22 $ $Id: survexp.s,v 4.2 1992-03-08 20:19:22 therneau Exp $
+#SCCS $Id: survexp.s,v 4.3 1992-03-09 00:17:17 therneau Exp $
 surv.exp <- function(entry, birth, sex, times=round(182.6 * 0:8),
 		      type=c("mean", "individual", "matrix"),
 		      expected=surv.exp.uswhite, interp=F) {
@@ -27,15 +27,16 @@ surv.exp <- function(entry, birth, sex, times=round(182.6 * 0:8),
     sex <- sex[nomiss]
     nused <- sum(nomiss)
     if (nused==0) stop ("No data remains after deleting missing values")
-    if (any(nomiss)) attr(nused, 'omit') <- seq(nn)[!nomiss]
+    if (any(!nomiss)) attr(nused, 'omit') <- seq(nn)[!nomiss]
 
     if (!inherits(birth, "date")) {
 	# Assume that they gave an age
 	if (any(birth <=0)) stop ("Age must be >0 ")
 	if (any(birth >130))stop ("Impossibly large value for age at entry")
-	birth <- entry - round(birth*365.25)
+	birth <- entry - round(birth*365.2425)
 	}
 
+    if (any(entry<birth)) stop("Subject entered before birth")
     if (any(times<0)) stop ("'Times' must be >=0")
     if (type != 'individual') times <- as.integer(sort(unique(times)))
     ntime <- length(times)
@@ -44,7 +45,7 @@ surv.exp <- function(entry, birth, sex, times=round(182.6 * 0:8),
     dd <- dim(expected)
     if (is.null(dd) || length(dd) <2 || dd[2]<2 || length(dd) >3 )
 	stop("The hazard table is not in the correct format")
-    ages <- as.integer(round(365.25 *as.numeric(dn[[1]])))
+    ages <- as.integer(round(365.2425 *as.numeric(dn[[1]])))
     if (ages[1] !=0  ||  length(ages)<2 || any(diff(ages)<=0))
 	stop("The hazard table is not in the correct format")
 
@@ -81,9 +82,8 @@ surv.exp <- function(entry, birth, sex, times=round(182.6 * 0:8),
     else                  nsurv <- nused
     temp <-  .C("survexp", as.integer(ntime),
 			    as.integer(times),
-			    as.integer(dd[1]),
+			    as.integer(dd),
 			    ages,
-			    as.integer(dd[3]),
 			    as.integer(years),
 			    as.single(expected),
 			    as.integer(nused),
@@ -101,8 +101,8 @@ surv.exp <- function(entry, birth, sex, times=round(182.6 * 0:8),
 	}
     if (type != 'matrix') xx <- list(time=times, surv=temp$surv, n=nused)
     else     xx <-  list(time=times,
-			 surv=matrix(temp$surv, nrow=nused, byrow=T),
-			 n=used)
+			 surv=matrix(temp$surv, ncol=nused, byrow=F),
+			 n=nused)
     xx$call <- call
     if (type == 'individual') {
 	if (any(!nomiss)) {
