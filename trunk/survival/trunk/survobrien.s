38db91684ff33e11b82dfe9c6c160675c66b6109
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-03-09 01:33:11 +0000

    First working version, under the new "formula" style
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9717 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survobrien.s
+++ trunk/survival/trunk/survobrien.s
@@ -1,37 +1,48 @@
-#SCCS $Date: 1992-03-04 16:48:38 $ $Id: survobrien.s,v 4.1 1992-03-04 16:48:38 therneau Exp $
-# SCCS  $Id: survobrien.s,v 4.1 1992-03-04 16:48:38 therneau Exp $
+# SCCS  $Id: survobrien.s,v 4.2 1992-03-09 01:33:11 therneau Exp $
 #
 # The test for survival proposed by Peter O'Brien
 #
-surv.obrien <- function(formula) {
-    m <- model.frame(formula, na.action= function(x) x )
+surv.obrien <- function(formula, data= sys.parent()) {
+    m <- model.frame(formula, data, na.action= function(x) x )
     n <- nrow(m)
+    Terms <- attr(m, 'terms')
 
+    y <- model.extract(m, 'response')
+    if (!inherits(y, "Surv")) stop ("Response must be a survival object")
+    if (attr(y, 'type') != 'right') stop("Can only handle right censored data")
 
-    num <- rep(0, nvar)
-    denom <- num
+    # Figure out which are the continuous predictor variables
+    m.name <- names(m)
+    temp <- match(attr(Terms, 'term.labels'), m.name)
+    cont <- NULL
+    for (i in temp) {if (!is.factor(m[[i]])) cont <- c(cont, i)}
+    if (is.null(cont)) stop ("No continuous variables to modify")
+
+    keepers <- rep(T, length(m))  #The ones to be kept "as is"
+    keepers[cont] <- F
+    keepers[attr(Terms, 'response')] <- F
+    ord <- order(y[,1])
+    x <- as.matrix(m[ord, cont])
+    time <- y[ord,1]
+    status <- y[ord,2]
+    nvar <- length(cont)
+
     nline <- 0
     for (i in unique(time[status==1])) nline <- nline + sum(time >=i)
     start <- stop <- event <- double(nline)
     xx <- matrix(double(nline*nvar), ncol=nvar)
     ltime <- 0
     j<- 1
-
+    keep.index <- NULL
     for (i in unique(time[status==1])) {
 	who <- (time >=i)
 	nrisk <- sum(who)
 	if (nrisk<2) break
-	temp <- apply(x[who,], 2, rank)
+	temp <- apply(x[who,,drop=F], 2, rank)
 	temp <- (2*temp -1)/ (2* nrisk)   #percentiles
 	logit<- log(temp/(1-temp))           #logits
 	deaths <- (status[who]==1 & time[who]==i)
-     # The mean logit is deterministicly=0, hence the (n-1)/n adjustment
-     #    (If there are ties it may not be zero).
-	denom <- denom + sum(deaths)*apply(logit,2,var)*(nrisk-1)/nrisk
-	if (sum(deaths) >1) num <- num + apply(logit[deaths,], 2, sum)
-	else                num <- num + logit[deaths,]
 
-	#Multivariate case
 	k <- seq(from=j, length=nrisk)
 	start[k] <- ltime
 	stop[k] <-  i
@@ -39,10 +50,18 @@ surv.obrien <- function(formula) {
 	xx[k,] <- logit
 	j <- j + nrisk
 	ltime <- i
+	keep.index <- c(keep.index, ord[who])
 	}
-    dimnames(xx) <- list(NULL, dimnames(x)[[2]])
-    temp <- agreg(start, stop, event, xx)
-    temp <- list(num=num, denom=denom, agfit=temp)
-    attr(temp, "class") <- 'surv.obrien'
+
+    if (any(keepers)) {
+	 temp <- list(m[keep.index, keepers], start, stop, event, xx)
+	 names(temp) <- c(m.name[keepers], "start", "stop", "event",
+				m.name[cont])
+	 }
+    else {
+	temp <- list(start, stop, event, xx)
+	names(temp) <- c(m.name[keepers], "start", "stop", "event",
+				m.name[cont])
+	}
     temp
     }
