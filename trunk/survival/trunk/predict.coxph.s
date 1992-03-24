c50aeb78b05e64961bed5b25893cd1658d4e8969
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-03-24 22:08:43 +0000

    Spurious error msg if called without "type" arg
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9729 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/predict.coxph.s
+++ trunk/survival/trunk/predict.coxph.s
@@ -1,10 +1,11 @@
-#SCCS $Date: 1992-03-04 16:48:14 $ $Id: predict.coxph.s,v 4.1 1992-03-04 16:48:14 therneau Exp $
+#SCCS $Date: 1992-03-24 22:08:43 $ $Id: predict.coxph.s,v 4.2 1992-03-24 22:08:43 therneau Exp $
 predict.coxreg <-
 function(object, newdata, type=c("lp", "risk", "expected", "terms"),
 		se.fit=F,
 		terms=labels(object), miss.expand=T, collapse, ...)
 
     {
+    type <-match.arg(type)
     if (type=='terms') {
 	tt <- object$terms
 	sp <- attr(tt, 'specials')$strata
