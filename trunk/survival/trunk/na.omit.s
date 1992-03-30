46a1a16fd092e2ae058732ffd881e8b465aab8bd
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-03-30 02:37:45 +0000

    Add naprint and naresid methods.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9745 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/na.omit.s
+++ trunk/survival/trunk/na.omit.s
@@ -1,4 +1,4 @@
-#SCCS $Date: 1992-03-04 16:48:10 $ $Id: na.omit.s,v 4.1 1992-03-04 16:48:10 therneau Exp $
+#SCCS $Date: 1992-03-30 02:37:45 $ $Id: na.omit.s,v 4.2 1992-03-30 02:37:45 therneau Exp $
 na.omit <- function(frame)  {
     n <- length(frame)
     omit <- FALSE
@@ -20,7 +20,43 @@ na.omit <- function(frame)  {
     if (any(omit)) {
 	temp <- seq(omit)[omit]
 	names(temp) <- row.names(frame)[omit]
-	attr(xx, "omit") <- temp
+	attr(temp, 'class') <- 'omit'
+	attr(xx, "na.action") <- temp
 	}
     xx
+    }
+
+naprint.omit <- function(x)
+    paste(length(x), "deleted due to missing")
+
+# Put the missing values back into a vector.
+#   And be careful about the labels too.
+naresid.omit <- function(omit, x) {
+    if (!length(omit) || !is.numeric(omit))
+	stop("Invalid argument for 'omit'")
+
+    if (is.matrix(x)) {
+	n <- nrow(x)
+	keep <- rep(NA,n+ length(omit))
+	keep[-omit] <- 1:n
+	x <- x[keep,,drop=F]
+	temp <- dimnames(x)[[1]]
+	if (length(temp)) {
+	    temp[omit] <- names(omit)
+	    dimnames(x) <- list(temp, dimnames(x)[[2]])
+	    }
+	x
+	}
+    else {
+	n <- length(x)
+	keep <- rep(NA,n+ length(omit))
+	keep[-omit] <- 1:n
+	x <- x[keep]
+	temp <- names(x)
+	if (length(temp)) {
+	    temp[omit] <- names(omit)
+	    names(x) <- temp
+	    }
+	x
+	}
     }
