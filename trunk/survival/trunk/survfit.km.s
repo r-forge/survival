531e071a440dd9db850d66970124da0e3373e330
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-04-13 22:38:50 +0000

    Initial revision
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9774 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/survival/trunk/survfit.km.s
@@ -0,0 +1,89 @@
+#SCCS $Id: survfit.km.s,v 4.1 1992-04-13 22:38:50 therneau Exp $
+survfit.km <- function(x, y, casewt=rep(1,n),
+	    type=c('kaplan-meier', 'fleming-harrington'),
+	    error=c('greenwood', "tsiatis"), se.fit=T,
+	    conf.int= .95,
+	    conf.type=c('log', 'log-log', 'plain', 'none'))
+    {
+    type <- match.arg(type)
+    method <- match(type, c("kaplan-meier", "fleming-harrington"))
+
+    error <- match.arg(error)
+    error.int <- match(error, c("greenwood", "tsiatis"))
+    conf.type <- match.arg(conf.type)
+
+    ny <- ncol(y)
+    n <- nrow(y)
+
+    if (!is.Surv(y)) stop("y must be a Surv object")
+    if (!is.factor(x)) stop("x must be a factor")
+    if (attr(y, 'type') != 'right') stop("Can only handle right censored data")
+
+    sorted <- (1:n)[order(x, y[,ny-1])]
+    y <- y[sorted,]
+    newstrat <- as.numeric(x[sorted])
+    newstrat <- as.integer(c(1*(diff(newstrat)!=0), 1))
+    if (sum(newstrat) > n/2)
+	stop("Number of strata > number of observations/2")
+
+    storage.mode(y) <- "double"
+    surv <- .C("survfit", as.integer(n),
+			  y = y,
+			  as.integer(ny),
+			  as.double(casewt),
+			  strata= as.integer(newstrat),
+			  as.integer(method),
+			  as.integer(error.int),
+			  mark=integer(n),
+			  surv=double(n),
+			  varhaz=double(n),
+			  risksum=double(n),
+			  ntime = integer(1))
+    ntime <- surv$ntime
+    if (error.int==1) surv$varhaz[surv$surv==0] <- NA
+    ntime <- 1:ntime
+    if (surv$strata[1] ==1)
+	temp _ list(time=surv$y[ntime,1],
+		 n.risk=surv$risksum[ntime],
+		 n.event=surv$mark[ntime],
+		 surv=surv$surv[ntime])
+    else {
+	temp <- surv$strata[1:(1+surv$strata[1])]
+	tstrat <- diff(c(0, temp[-1])) #n in each strata
+	names(tstrat) <- levels(x)
+	temp _ list(time=surv$y[ntime,1],
+		 n.risk=surv$risksum[ntime],
+		 n.event=surv$mark[ntime],
+		 surv=surv$surv[ntime],
+		 strata= tstrat)
+	}
+
+    if (se.fit) {
+	temp$std.err <- sqrt(surv$varhaz[ntime])
+	zval _ qnorm(1- (1-conf.int)/2, 0,1)
+	if (conf.type=='plain') {
+	    temp1 <- temp$surv + zval* temp$std * temp$surv
+	    temp2 <- temp$surv - zval* temp$std * temp$surv
+	    temp <- c(temp, list(upper=pmin(temp1,1), lower=pmax(temp2,0),
+			    conf.type='plain', conf.int=conf.int))
+	    }
+	if (conf.type=='log') {
+	    xx <- ifelse(temp$surv==0,1,temp$surv)  #avoid some "log(0)" messages
+	    temp1 <- ifelse(temp$surv==0, 0*temp$std, exp(log(xx) + zval* temp$std))
+	    temp2 <- ifelse(temp$surv==0, 0*temp$std, exp(log(xx) - zval* temp$std))
+	    temp <- c(temp, list(upper=pmin(temp1,1), lower=temp2,
+			    conf.type='log', conf.int=conf.int))
+	    }
+	if (conf.type=='log-log') {
+	    who <- (temp$surv==0 | temp$surv==1) #special cases
+	    xx <- ifelse(who, .1,temp$surv)  #avoid some "log(0)" messages
+	    temp1 <- exp(-exp(log(-log(xx)) + zval*temp$std/log(xx)))
+	    temp1 <- ifelse(who, temp$surv + 0*temp$std, temp1)
+	    temp2 <- exp(-exp(log(-log(xx)) - zval*temp$std/log(xx)))
+	    temp2 <- ifelse(who, temp$surv + 0*temp$std, temp2)
+	    temp <- c(temp, list(upper=temp1, lower=temp2,
+			    conf.type='log-log', conf.int=conf.int))
+	    }
+	}
+    temp
+    }
---
+++ trunk/survival/trunk/survfit.s
@@ -0,0 +1,55 @@
+#SCCS $Date: 1992-04-13 22:38:32 $ $Id: survfit.s,v 4.1 1992-04-13 22:38:32 therneau Exp $
+survfit <- function (formula, data, weights, subset, na.action, ...) {
+    call <- match.call()
+    # Real tricky -- find out if the first arg is "Surv(...)" without
+    #  evaluating it.  If this is so, or it is a survival object, turn it
+    #  into a formula
+    if ((mode(call[[2]]) == 'call' &&  call[[2]][[1]] == as.name('Surv'))
+		|| attr(formula, 'class') == 'Surv') {
+	# The dummy function stops an annoying warning message "Looking for
+	#  'formula' of mode function, ignored one of mode ..."
+	xx <- function(x) formula(x)
+	formula <- xx(paste(deparse(call[[2]]), 1, sep="~"))
+	}
+
+    if (!inherits(formula, 'formula')) temp <- NextMethod("survfit")
+    else {
+	m <- match.call(expand=F)
+	m$... <- NULL
+
+	Terms <- terms(formula, 'strata')
+	ord <- attr(Terms, 'order')
+	if (length(ord) & any(ord !=1))
+	    stop("Interaction terms are not valid for this function")
+	m$formula <- Terms
+	m[[1]] <- as.name("model.frame")
+	m <- eval(m, sys.parent())
+
+	n <- nrow(m)
+	Y <- model.extract(m, response)
+	casewt <- model.extract(m, "weights")
+	# The second line below works around a bug in Splus 3.0.1, which later
+	#    went away, i.e., casewt is returned as an unevaluated arg.
+	if (is.null(casewt)) casewt <- rep(1,n)
+	else if (mode(casewt)=='argument') casewt <- eval(casewt[[1]])
+
+	ll <- attr(Terms, 'term.labels')
+	if (length(ll) == 0) X <- factor(rep(1,n))
+	else {
+	    temp <-  rep(1, length(ll))
+	    strat <- attr(Terms, 'specials')$strata
+	    if (length(strat)) temp[strat] <- 0
+	    lname <- ifelse(temp==1, paste(ll,'=', sep=''), "")
+	    temp <- paste("'",lname, "', ","as.character(m[['", ll, "']])", sep='')
+	    temp <- paste(temp, collapse=", ', ',")
+	    temp <- paste("paste(", temp, ",sep='')")
+	    X <- factor(eval(parse(text=temp)))
+	    }
+
+	temp <- survfit.km(X, Y, casewt, ...)
+	attr(temp, "class") <- "survfit"
+	if (!is.null(attr(m, 'na.action'))) temp$na.action <- attr(m, 'na.action')
+	}
+    temp$call <- call
+    temp
+    }
