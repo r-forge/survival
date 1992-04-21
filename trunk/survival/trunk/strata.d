2bd7e96cc71f112ce403e924dc3e5891d5c43132
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-04-21 15:36:29 +0000

    Edit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9791 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/strata.d
+++ trunk/survival/trunk/strata.d
@@ -1,68 +1,29 @@
 .BG
 .FN strata
 .TL
-~function to do ???
+Identify strata variables.
 .DN
-~brief description
+This is a special function used in the context of the Cox model.  It
+identifies stratification variables when they appear on the right hand
+side of a formula.
 .CS
 strata(..., na.group=F)
 .RA
-.OA
-~move the above line to just above the first optional argument
 .AG ...
-~Describe ... here
+Any number of variables.  All must be the same length.
+.OA
 .AG na.group
-~Describe na.group here
+if set to T, then missing values are treated as a distinct level of each
+variable.
 .RT
-~Describe the value returned
-.SE
-~describe any side effects if they exist
+a new factor, whose levels are all possible combinations of the factors
+supplied as arguments.
 .DT
-~explain details here.
-.SH REFERENCES
-~put references here, make other sections like NOTE and WARNING with .SH
+The result is identical to the interaction() function, but for the
+labeling of the factors (strata is more verbose).
 .SA
-~put functions to SEE ALSO here
+coxph
 .EX
-# The function is currently defined as
-function(..., na.group = F)
-{
-	words <- match.call()
-	allf <- list(...)
-	if(length(allf) == 1 && is.list(ttt <- unclass(allf[[1]])))
-		allf <- ttt
-	nterms <- length(allf)
-	what <- allf[[nterms]]
-	if(is.null(levels(what)))
-		what <- factor(what)
-	levs <- unclass(what) - 1
-	wlab <- levels(what)
-	if(na.group && any(is.na(what))) {
-		levs[is.na(levs)] <- length(wlab)
-		wlab <- c(wlab, "NA")
-	}
-	labs <- paste(words[nterms], wlab, sep = "=")
-	for(i in (1:nterms)[ - nterms]) {
-		what <- allf[i]
-		if(is.null(levels(what)))
-			what <- factor(what)
-		wlab <- levels(what)
-		wlev <- unclass(what) - 1
-		if(na.group && any(is.na(wlev))) {
-			wlev[is.na(wlev)] <- length(wlab)
-			wlab <- c(wlab, "NA")
-		}
-		wlab <- paste(words[i], wlab, sep = "=")
-		levs <- levs * length(wlab) + wlev
-		labs <- as.vector(outer(wlab, labs, paste, sep = ", "))
-	}
-	levs <- levs + 1
-	ulevs <- unique(levs[!is.na(levs)])
-	levs <- match(levs, ulevs)
-	labs <- labs[ulevs]
-	levels(levs) <- labs
-	class(levs) <- "factor"
-	levs
-}
-.KW ~keyword
+coxph(Surv(futime, fustat) ~ age + strata(rx))
+.KW survival
 .WR
