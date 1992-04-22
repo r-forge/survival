290fc1523056c5fa9bc11733d280463ce86540fb
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-04-22 12:29:23 +0000

    Edit
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9794 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/residuals.coxph.d
+++ trunk/survival/trunk/residuals.coxph.d
@@ -3,61 +3,43 @@
 .TL
 Calculate residuals for a coxph fit.
 .DN
-Calculates martingale, deviance, score or schoenfeld residuals for
-Cox or Anderson-Gill proportional hazards models.
+Calculates martingale, deviance, score or Schoenfeld residuals for a
+Cox proportional hazards model.
 .CS
-residuals.coxph(object, 
+resid(object,
        type=c("martingale", "deviance", "score", "schoenfeld"), 
-       miss.expand=T, collapse)
+       collapse)
 .RA
 .AG object
 a coxph object, output from a coxph fit.
 .OA
 .AG type
-character string indicating the type of residual desired.  Default is ???.
+character string indicating the type of residual desired;
+the default is martingale.
 Only enough of the string to determine a unique match is required.
-.AG miss.expand
-Should the function return one residual for each row (miss.expand == T)
-or for each row without NA's (miss.expend == F)?  Default is TRUE.
-Note: argument is only relevant when data set contains missing values and 
-when na.action=='naomit'.
 .AG collapse
 Vector indicating which rows to collapse(sum) over.  In time-dependent
-models more than one row can pertain to the one individual.  If collapse
-is given a vector indicating which rows correspond the the same individuals
-one residual for each individual will be returned.
+models more than one row data can pertain to a single individual.
+If there were 4 individuals represented by 3, 1, 2 and 4 rows of data
+respectively, then `collapse=c(1,1,1, 2, 3,3, 4,4,4,4)' could be used to
+obtain per subject rather than per observation residuals.
 .RT
 The object returned will be a vector for martingale and deviance 
 residuals and matrices for score and schoenfeld residuals.  There will
-be one row of residuals for each row in the input unless miss.expand=F
-or collapse is specified.  One column of score and schoenfeld 
+be one row of residuals for each row in the input data (without `collapse').
+One column of score and Schoenfeld
 residuals will be returned for each column in the model.matrix.
 .SE
-~describe any side effects if they exist
-.DT
-~explain details here.
+For deviance residuals, the status variable may need to be reconstructed.
+For score and Shoenfeld residuals, the X matrix will need to be reconstructed.
 .SH REFERENCES
-Terry Therneau, author of local function.
-
-P. Andersen and R. Gill. "Cox's regression model for
-counting processes, a large sample study", Annals of Statistics, 
-10:1100-1120, 1982.  
-
 T.Therneau, P. Grambsch, and T.Fleming. "Martingale based residuals
 for survival models", Biometrika, March 1990.
 .SA
-coxph.
+coxph
 .EX
-
-#
-# An example of how to manage the residuals
-#
-attach(jasa1)
+> attach(jasa1)
 > fit <- coxph(Surv(start, stop, event) ~ (age + surgery)* transplant)
-#
-# The true residual for a subject is the sum of the rows that were used to
-#   represent him/her to coxph.
 > mresid <- resid(fit, collapse=jasa1$id)
-
-.KW ~keyword
+.KW survival
 .WR
