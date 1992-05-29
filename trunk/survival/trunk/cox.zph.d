837957e8ed2130d1acc6d30c6338f39bfc49ce34
sicks <unknown@noreply.com>
1992-05-29 09:10:03 +0000

    interchange rows and columns to reflect change in function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9809 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/cox.zph.d
+++ trunk/survival/trunk/cox.zph.d
@@ -7,26 +7,28 @@ cox.zph(fit, ranks=T, global=T)
 .AG fit
 the result of fitting a Cox regression model, using the coxph function.
 .AG ranks
-if true, survival times are replaced by thier ranks before the test is
+if true, survival times are replaced by their ranks before the test is
 performed.
 .AG global
 do the global test as well as the per-variable tests.
 .RT
-a matrix with one column for each variable and (optionally) a column for
+a matrix with one row for each variable and (optionally) a row for
 the global test.
-Rows of the matrix contain the correlation coefficent between survival time
-or rank survival time and the Shoenfeld residuals, a normal score (based on
+Columns of the matrix contain the correlation coefficient between survival time
+or rank survival time and the Schoenfeld residuals, a normal score (based on
 Fisher's Z transform), and the two-sided p value.
 .pp
 This function would usually be executed in conjunction with a plot of the
-Shoenfeld residuals versus survival time.
+Schoenfeld residuals versus survival time.
 .SH METHOD
 The global test is based on the linear predictor lp <- X %*% coef.  It is
 equivalent to the univariate test on a new fit with lp as the
 covariate.  If the original model has only a single predictor, then the
 global test is just a repeat of the univariate test for that predictor.
+.SH REFERENCES
+Terry Therneau, author of local function.
 .EX
-fit <- coxph(Surv(futime, fustat) ~ age + surgery, jasa)
+fit <- coxph( Surv(futime, fustat) ~ age + surgery, jasa)
 cox.zph(fit)
 .KW survival
 .WR
