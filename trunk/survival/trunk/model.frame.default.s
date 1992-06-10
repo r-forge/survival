aa1604b0b2e9530da096bcfa53ef9097caf3215d
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-06-10 20:47:36 +0000

    Work around a bug in Splus 3.0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9813 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/model.frame.default.s
+++ trunk/survival/trunk/model.frame.default.s
@@ -1,4 +1,4 @@
-#SCCS $Date: 1992-04-14 18:06:49 $ $Id: model.frame.default.s,v 4.2 1992-04-14 18:06:49 grill Exp $
+#SCCS $Date: 1992-06-10 20:47:36 $ $Id: model.frame.default.s,v 4.3 1992-06-10 20:47:36 therneau Exp $
 # Only change -- look to options() for the default na.action
 #
 model.frame.default <- function(formula, data = NULL, na.action = na.fail, ...)
@@ -23,5 +23,19 @@ model.frame.default <- function(formula, data = NULL, na.action = na.fail, ...)
     if(!inherits(formula, "terms"))
         formula <- terms(formula, data = data)
     dots <- substitute(list(...))
-    .Internal(model.frame(formula, data, na.action, dots), "model_frame")
+# Work around a bug in Splus 3.0 -- doesn't check that the objects are
+#   all the same length.  Later fixed in Bell S.  Someday replace all of
+#   this by the original, commented line below.
+#   .Internal(model.frame(formula, data, na.action, dots), "model_frame")
+    temp <- .Internal(model.frame(formula, data, na.action, dots), "model_frame")
+    if (is.matrix(temp[[1]])) n <- nrow(temp[[1]])
+	else                  n <- length(temp[[1]])
+    j <- 1
+    for (i in temp[-1]) {
+	j <- j+1
+	mat <- is.matrix(i)
+	if ((mat && n!=nrow(i)) ||  (!mat && n!=length(i)))
+	    stop(paste("Length mismatch, term", j))
+	}
+    temp
     }
