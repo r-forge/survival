20cfffde05e3cfd99b3303c2ebed479aceb70a43
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-06-24 09:35:11 +0000

    Initial revision
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9815 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/survival/trunk/print.coxph.s
@@ -0,0 +1,37 @@
+# SCCS $Id: print.coxph.s,v 4.1 1992-06-24 09:34:12 therneau Exp $
+print.coxph <-
+ function(cox, digits=3, ...)
+    {
+    if (!is.null(cl<- cox$call)) {
+	cat("Call:\n")
+	dput(cl)
+	cat("\n")
+	}
+    if (!is.null(cox$fail)) {
+	cat(" Coxreg failed.", cox$fail, "\n")
+	return()
+	}
+    savedig <- options(digits = digits)
+    on.exit(options(savedig))
+
+    coef <- cox$coef
+    se <- sqrt(diag(cox$var))
+    if(is.null(coef) | is.null(se))
+        stop("Input is not valid")
+    tmp <- cbind(coef, exp(coef), se, coef/se, 1 - pchisq((coef/
+	se)^2, 1))
+    dimnames(tmp) <- list(names(coef), c("coef", "exp(coef)",
+	"se(coef)", "z", "p"))
+    prmatrix(tmp)
+
+    logtest <- -2 * (cox$loglik[1] - cox$loglik[2])
+    df <- length(coef)
+    cat("\n")
+    cat("Likelihood ratio test=", format(round(logtest, 2)), "  on ",
+	df, " df,", " p=", format(1 - pchisq(logtest, df)),  sep="")
+    omit <- cox$na.action
+    if (length(omit))
+	cat("  n=", cox$n, " (", naprint(omit), ")\n", sep="")
+    else cat("  n=", cox$n, "\n")
+    invisible()
+    }
