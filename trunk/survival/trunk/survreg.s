c006532520e17369662b694092430490fbee3bc3
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-06-26 23:27:25 +0000

    Complete rewrite
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9816 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survreg.s
+++ trunk/survival/trunk/survreg.s
@@ -1,14 +1,14 @@
-#SCCS $Date: 1992-04-14 18:08:12 $ $Id: survreg.s,v 4.4 1992-04-14 18:08:12 grill Exp $
+#SCCS $Date: 1992-06-26 23:27:25 $ $Id: survreg.s,v 4.5 1992-06-26 23:27:25 therneau Exp $
 survreg <- function(formula=formula(data), data=sys.parent(),
 	subset, na.action,
+	link=c('log', 'identity'), dist=c("extreme", "logistic", "exp"),
+	scale,
 	eps=.0001, init, iter.max=10,
-	method= c("weibull"),
 	model=F, x=F, y=F, ...) {
 
-    method <- match.arg(method)
     call <- match.call()
     m <- match.call(expand=F)
-    m$method <- m$model <- m$x <- m$y <- m$... <-  NULL
+    m$dist <- m$link <- m$model <- m$x <- m$y <- m$... <-  NULL
     m$eps <- m$inf.ratio <- m$init <- m$iter.max <- m$n.table <- NULL
 
     Terms <- if(missing(data)) terms(formula, 'strata')
@@ -16,37 +16,71 @@ survreg <- function(formula=formula(data), data=sys.parent(),
     m$formula <- Terms
     m[[1]] <- as.name("model.frame")
     m <- eval(m, sys.parent())
-    if (method== 'model.frame') return(m)
 
+    dist <- match.arg(dist)
+    link <- match.arg(link)
     Y <- model.extract(m, "response")
     if (!inherits(Y, "Surv")) stop("Response must be a survival object")
+    X <- model.matrix(Terms, m)
+    n <- nrow(X)
+    nvar <- ncol(X)
     offset<- attr(Terms, "offset")
     if (!is.null(offset)) offset <- as.numeric(m[[offset]])
-    X <- model.matrix(Terms, m)
+    else                  offset <- rep(0, n)
 
     type <- attr(Y, "type")
-    if (type!='right' && type!='counting') stop ("Invalid survival type")
+    linkfun <- glm.links["link", link][[1]]
+    if (type== 'counting') stop ("Invalid survival type")
+    else if (type=='interval') {
+	if (any(Y[,3]==4))
+	     Y <- cbind(linkfun(Y[,1:2]), Y[,3])
+	else Y <- cbind(linkfun(Y[,1]), Y[,3])
+	}
+    else     Y <- cbind(linkfun(Y[,1]), Y[,2])
 
-    if( method=="weibull") fitter <- weibull.fit
-    else stop(paste ("Unknown method", method))
-
-    casewt <- rep(1,ncol(X))
     if (missing(init)) init <- NULL
-    fit <- fitter(Y, X, casewt, offset, iter.max=iter.max,
-			eps=eps, init=init, ...)
+    if (missing(scale)) scale <- NULL
 
-    if (is.character(fit))  fit <- list(fail=fit)
+    if( dist=="extreme")
+	sfit <- survreg.fit(X, Y, offset, init=init, iter.max=iter.max,
+			    eps = eps, fun='exvalue', scale=scale, ...)
+    else if (dist=="logistic")
+	sfit <- survreg.fit(X, Y, offset, init=init, iter.max=iter.max,
+			    eps = eps, fun='logisticfit', scale=scale, ...)
+    else if (dist=="exp")
+	sfit <- survreg.fit(X, Y, offset, init=init, iter.max=iter.max,
+			    eps = eps, fun='exvalue', scale=1, ...)
+
+    else stop(paste ("Unknown method", dist))
+
+    if (is.character(sfit))  fit <- list(fail=sfit)  #error message
     else {
-	fit$n <- nrow(Y)
+	# There are certainly more clever ways to do this, but ....
+	#  In order to make it look like IRLS, and get all the components
+	#  that I need for glm inheritance, do one step of weighted least
+	#  squares.
+	if (missing(scale)) scale <- sfit$coef[nvar+1]
+	eta <- c(X %*% sfit$coef[1:nvar]) + offset
+	wt<- -sfit$deriv[,3]/scale^2
+	fit <- lm.wfit(X, eta + sfit$deriv[,2]/(scale*wt), wt, "qr", ...)
+
 	na.action <- attr(m, "na.action")
 	if (length(na.action)) fit$na.action <- na.action
+	if (link=='log') fit$fitted.values <- exp(fit$fitted.values)
+	fit$family <- c(name=dist, link=link, "")
+	fit$linear.predictors <- eta
+	fit$deviance <- -2*sfit$loglik[1]
+	fit$null.deviance <- -2*sfit$null[1]
+	fit$iter <- sfit$iter
+	fit$var  <- solve(sfit$imat)
+	fit$flag <- sfit$flag
+	fit$dresiduals <- sign(fit$residuals)*sqrt(-2*sfit$deriv[,1])
 	}
 
-    attr(fit, "class") <-  c(method, "survreg", "lm")
+    attr(fit, "class") <-  c("survreg", "glm", "lm")
     fit$terms <- Terms
     fit$formula <- as.vector(attr(Terms, "formula"))
     fit$call <- call
-    fit$assign <- attr(X, 'assign')
     if (model) fit$model <- m
     if (x)     fit$x <- X
     if (y)     fit$y <- Y
