4f41da490162276b20a7433691d47823bd9d8787
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-07-13 22:00:59 +0000

    Initial revision
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9829 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/survival/trunk/survreg.d
@@ -0,0 +1,47 @@
+.BG
+.FN survreg
+.TL
+Regression for a parametric survival model
+.CS
+survreg(formula, data=sys.parent(), subset, na.action,
+link=c("log", "identity"),
+dist=c("extreme", "logistic", "gaussian", "exponential"),
+scale, eps=0.0001, init, iter.max=10, model=F, x=F, y=F, ...)
+.AG formula
+a formula expression as for other regression models.
+See the documentation for `lm' and `formula' for details.
+.AG data
+optional data frame in which to interpret the variables occuring in the
+formula.
+.AG subset
+subset of the observations to be used in the fit.
+.AG na.action
+function to be used to handle any NAs in the data.
+.AG link
+transformation to be used on the y variable.
+.AG dist
+assumed distribution for the transformed y variable.
+.AG scale
+optional; gives a fixed value for the scale parameter.
+.AG eps
+convergence criteria for the computation.  Iteration continues until the
+relative change in log likelihood is less than eps.
+.AG init
+optional vector of initial values for the paramters.
+.AG iter.max
+maximum number of iterations to be performed.
+.AG model
+if TRUE, the model frame is returned.
+.AG x
+if TRUE, then the X matrix is returned.
+.AG y
+if TRUE, then the y vector (or survival times) is returned.
+.AG ...
+all the optional arguments to lm, including `singular.ok'.
+.RT
+an object of class `survreg' is returned, which inherits from class `glm'.
+.EX
+survreg(Surv(futime, fustat) ~ ecog.ps + rx, fleming, dist='extreme',
+		link='log', scale=1)   #Fit an exponential
+.KW survival
+.WR
