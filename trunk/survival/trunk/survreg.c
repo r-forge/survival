b7cfd352ffd8b0664bf268e05bc22f7c6e9ccefb
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1992-11-16 14:50:56 +0000

    Complete rewrite, to allow multiple distributions easily.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9891 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survreg.c
+++ trunk/survival/trunk/survreg.c
@@ -1,4 +1,4 @@
-/* SCCS $Id: survreg.c,v 4.2 1992-09-24 13:35:06 therneau Exp $    */
+/* SCCS @(#)survreg.c	4.2 9/24/92    */
 /*
 ** Fit one of several censored data distributions
 **
@@ -13,13 +13,11 @@
 **                   Note that S sends this in column major order
 **      offset  - offset vector (usually 0)
 **      beta    - initial values for the parameters
-**      scale   - if =0, then estimate the scale, otherwise this specifies
-**                  the scale.  If =0, then coef will be of length nvar+1.
-**                  Note that beta will contain the log(scale).
-**      doinit  - if >0, then beta does NOT contain initial values.  The
-**                  routine will do a 'glm type' initialization.  If scale is
-**                  to be estimated, however, beta still DOES contain a
-**                  starting guess for that parameter.
+**      np      - number of "extra" parameters in the distribution.  Always
+**                  >=1.
+**      pars    - An array of length npar*2.  The first column contains
+**                  initial parameter estimates, the second is =1 if this
+**                  parameter is fixed, i.e. not part of the iteration.
 **      eps     - tolerance for convergence.  Iteration continues until the
 **                  relative change in the deviance is <= eps.
 **      dist    -  1=extreme value, 2=logistic, 3=gaussian, 4=cauchy
@@ -28,7 +26,6 @@
 **      beta    - the final coef vector
 **      maxiter - the number of iterations consumed
 **      imat    - the information matrix
-**      jmat    - alternate estimate based on U'U
 **      loglik  - the final deviance (log lik + constant)
 **      flag    - success flag  0 =ok
 **                              -1= did not converge
@@ -59,7 +56,7 @@
 #define  SPI    2.506628274631001     /* sqrt(2*pi) */
 #define  ROOT_2 1.414213562373095
 
-static int debug =0;       /* normally set to zero */
+static int debug =1;       /* normally set to zero */
 
 double **dmatrix();
 static void exvalue_d();
@@ -70,58 +67,55 @@ static void (*sreg_gg)();
 void sreg_g();
 void sreg_deriv();
 
+static int    nvar, np;
 static double **covar;
 static double *time2, *time, *status;
 static double *g, *dg, *ddg;
 static double *dsig, *ddsig, *dsg;
 static double *offset;
-static double scale;
-static int glm_init;
+static double *parms, *pfixed;
 
 void survreg(maxiter, nx, nvarx, y, ny, covar2, offset2,
-		 beta, scalex, u, imatx, jmatx, loglik, flag, eps,
-		 deriv, do_init, dist)
+		 beta, npx, parmsx, u, imatx, loglik, flag, eps,
+		 deriv, dist)
 long    *nx,
 	*maxiter,
 	*nvarx,
 	*ny,
+	*npx,
 	*flag,
-	*dist,
-	*do_init;
+	*dist;
 double  *covar2,
 	*offset2,
-	*scalex,
+	*parmsx,
 	*deriv,
-	*imatx,
-	*jmatx;
+	*imatx;
 double  beta[],
 	*u,
 	*y,
 	loglik[2],
 	*eps;
     {
-    register int n, nvar, i;
+    register int n, nvar2, i;
     int maxiter2;
     double *newbeta,
 	   *savediag;
     double **imat;
-    double **jmat;
-    double temp, eta;
-    double temp2, temp3;
-    double best;
-    int    person;
 
     maxiter2 = *maxiter;
-    scale = *scalex;
     n = *nx;
     nvar = *nvarx;
+    np   = *npx;
     offset = offset2;
+    parms  = parmsx;
+    pfixed = parmsx + np;
+
+    nvar2 = nvar;
+    for (i=0; i<np; i++) if (pfixed[i]!=1) nvar2++;
+    imat = dmatrix(imatx, nvar2, nvar2);
     covar = dmatrix(covar2, n, nvar);
-    if (scale==0) nvar = nvar+1; /*sigma too */
-    imat = dmatrix(imatx, nvar, nvar);
-    jmat = dmatrix(jmatx, nvar, nvar);
-    newbeta = u+nvar;
-    savediag= newbeta + nvar;
+    newbeta = u+nvar2;
+    savediag= newbeta + nvar2;
 
     g = deriv;
     dg= g+n;
@@ -147,81 +141,14 @@ double  beta[],
 	case 4: sreg_gg = cauchy_d;   break;
 	}
 
-    if (*do_init>0) { /* set up a glm style initialization */
-	if (debug>0) fprintf(stderr, "Glm init-- scale=%f\n", scale);
-	if (scale==0) {
-	    nvar--;
-	    scale = exp(beta[nvar]);
-	    }
-	glm_init=1;
-	for (i=0; i<nvar; i++) beta[i]=0;
-	maxiter2 =0;
-	rnewton(&maxiter2, n, nvar, beta, u, imat, jmat, loglik, *eps,
-		    sreg_g, sreg_deriv, newbeta, savediag, debug);
-	for (i=0; i<nvar; i++) beta[i] = newbeta[i];
-
-	glm_init=0;
-	scale = *scalex;
-	if (scale==0) {
-	    temp =0;
-	    for (person=0; person<n; person++) {
-		eta =0;
-		for (i=0; i<nvar; i++) eta += beta[i] * covar[i][person];
-		eta += offset[person];
-		temp += (time[person] - eta) * (time[person] -eta);
-		}
-	    /* Dodge the case where temp==0  */
-	    beta[nvar] = log(.1*exp(beta[nvar]) + .9*temp/n);
-	    nvar++;
-	    }
-	maxiter2 = *maxiter;
-	}
-
-    *flag = rnewton(&maxiter2, n, nvar, beta, u, imat, jmat, loglik, *eps,
+    *flag = rnewton(&maxiter2, n, nvar2, beta, u, imat, loglik, *eps,
 		    sreg_g,  sreg_deriv, newbeta, savediag, debug);
     *maxiter = maxiter2;
-
-    /*
-    ** Do the deviance correction
-    */
-    if (scale==0) scale = exp(beta[nvar-1]);
-    temp =0;
-    for (person=0; person<n; person++) {
-	if (status[person]==1) switch(*dist) {
-	    case 1: best = -(1 + log(scale));
-		    break;
-	    case 2: best = -log(4*scale);   /*log(1/4) - log(sc..*/
-		    break;
-	    case 3: best = -log(SPI*scale);
-		    break;
-	    case 4: best = -log(PI*scale);
-	    }
-	else if (status[person]==3) {
-	    temp2= (time2[person] - time[person])/scale;
-	    switch(*dist) {
-		case 1: temp3= temp2/(exp(temp2)-1);
-			best = log(exp(-temp3) - exp(-exp(temp2)*temp3));
-			break;
-		case 2: temp3= exp(temp2/2);
-			best = log((temp3-1)/ (temp3+1));
-			break;
-		case 3: best = 2*erf(temp2/(2*ROOT_2));
-			break;
-		case 4: best = 2*atan(temp2/2)/PI  -1;
-		}
-	    }
-	else best=0;
-
-	g[person] = 2*(best - g[person]);
-	temp      += best;
-	}
-    loglik[0] = 2*(temp - loglik[0]);
-    loglik[1] = 2*(temp - loglik[1]);
     }
 
 
-void sreg_g(n, nvar, beta, loglik)
-int n, nvar;
+void sreg_g(n, nvar2, beta, loglik)
+int n, nvar2;
 double beta[];
 double *loglik;
     {
@@ -236,23 +163,17 @@ double *loglik;
     /*
     ** calculate the first and second derivative wrt eta
     */
-    if (scale>0) sigma = scale;
-    else {
-	nvar--;
-	sigma = exp(beta[nvar]);
-	}
+    if (pfixed[0]==1) sigma = exp(parms[0]);
+    else sigma = exp(beta[nvar]);
+    i= nvar;
+    for (j=0; j<np; j++) if (pfixed[j]!=1) parms[j] = beta[i++];
+
     *loglik =0;
     sig2 = 1/(sigma*sigma);
     for (person=0; person<n; person++) {
-	if (glm_init==1) {
-	    if (status[person]==3) eta= (time[person] + time2[person])/2;
-	    else                   eta= time[person];
-	    }
-	else {
-	    eta =0;
-	    for (i=0; i<nvar; i++) eta += beta[i] * covar[i][person];
-	    eta += offset[person];
-	    }
+	eta =0;
+	for (i=0; i<nvar; i++) eta += beta[i] * covar[i][person];
+	eta += offset[person];
 	z = (time[person] - eta) /sigma;
 
 	j = status[person];       /*convert to integer */
@@ -288,7 +209,7 @@ double *loglik;
 		dg[person]  = -(ufun[2] - funs[2])/(temp*sigma);
 		ddg[person] = (ufun[3] - funs[3])*sig2/temp -
 						     dg[person]*dg[person];
-		if (scale==0) {
+		if (pfixed[0]==0) {
 		    dsig[person] = (z*funs[2] - zu*ufun[2])/temp;
 		    ddsig[person]= ((zu*zu*ufun[3] - z*z*funs[3])
 				      +(zu*ufun[2] - z*funs[2])) /temp -
@@ -300,7 +221,6 @@ double *loglik;
 		break;
 	    }
 	*loglik += g[person];
-	if (glm_init==1) dg[person] -= eta*ddg[person];
 	}
     }
 
@@ -308,12 +228,11 @@ double *loglik;
 **  This routine works for all the routines that have ony a scale
 **   parameter as the "extra".
 */
-void sreg_deriv(n, nvar, beta, u, imat, jmat)
+void sreg_deriv(n, nvar2, beta, u, imat)
 int     n;
-int     nvar;
+int     nvar2;
 double  beta[],
 	u[],
-	**jmat,
 	**imat;
     {
     register int i,j, person;
@@ -322,30 +241,26 @@ double  beta[],
 	   sigma;
     double temp;
 
-    for (i=0; i<nvar; i++) {
+    for (i=0; i<nvar2; i++) {
 	u[i]=0;
-	for (j=0; j<nvar; j++) {
+	for (j=0; j<nvar2; j++)
 	    imat[i][j]=0;
-	    jmat[i][j]=0;
-	    }
 	}
-    if (scale>0) sigma = scale;
-    else {
-	nvar--;
-	sigma = exp(beta[nvar]);
-	}
+
+    if (pfixed[0]==1) sigma = parms[0];
+    else              sigma = exp(beta[nvar]);
+    i= nvar;
+    for (j=0; j<np; j++) if (pfixed[j]!=1) parms[j] = beta[i++];
 
     for (person=0; person<n; person++) {
 	for (i=0; i<nvar; i++) {
 	    u[i] += dg[person] * covar[i][person];
 	    for (j=0; j<=i; j++) {
 		imat[j][i] -= covar[i][person] *covar[j][person] *ddg[person];
-		jmat[j][i] += covar[i][person] *covar[j][person] *dg[person]*
-								  dg[person];
 		}
 	    }
 
-	if (scale==0) {   /* need derivative wrt log sigma */
+	if (pfixed[0]!=1) {   /* need derivative wrt log sigma */
 	    eta =0;
 	    for (i=0; i<nvar; i++) eta += beta[i] *covar[i][person];
 	    eta += offset[person];
@@ -355,7 +270,6 @@ double  beta[],
 		u[nvar] += dsig[person];
 		for (i=0; i<nvar; i++) {
 		    imat[i][nvar] -= dsg[person] * covar[i][person];
-		    jmat[i][nvar] += dsig[person]*dg[person]*covar[i][person];
 		    }
 		imat[nvar][nvar] -=  ddsig[person];
 		imat[nvar][nvar] +=  dsig[person]*dsig[person];
@@ -367,15 +281,109 @@ double  beta[],
 		for (i=0; i<nvar; i++) {
 		    imat[i][nvar] -= (sz*ddg[person] - dg[person]) *
 						covar[i][person];
-		    jmat[i][nvar] += temp*dg[person] * covar[i][person];
 		    }
 		imat[nvar][nvar] -= sz*sz*ddg[person] - sz*dg[person];
-		jmat[nvar][nvar] += temp*temp;
 		}
 	    }
 	}
     }
 
+void survreg_g(nx, y, ny, eta, beta, deriv, dist)
+long    *nx,
+	*ny,
+	*dist;
+double  eta[],
+	*deriv;
+double  beta[],
+	*y;
+    {
+    register int n;
+    register int person, j;
+    double  sigma;
+    double  z, zu,
+	    temp;
+    double  sig2;
+    static double  funs[4], ufun[4];
+
+    n = *nx;
+    g = deriv;
+    dg= g+n;
+    ddg = dg +n;
+    dsig= ddg +n;
+    ddsig = dsig +n;
+    dsg = ddsig +n;
+
+    if (*ny==2) {
+	time=y;
+	status = y+n;
+	}
+    else {
+	time=y;
+	time2 = time + n;
+	status = time2 +n;
+	}
+
+    switch(*dist) {
+	case 1: sreg_gg = exvalue_d;  break;
+	case 2: sreg_gg = logistic_d; break;
+	case 3: sreg_gg = gauss_d;    break;
+	case 4: sreg_gg = cauchy_d;   break;
+	}
+
+    /*
+    ** calculate the first and second derivative wrt eta
+    */
+    sigma = exp(beta[0]);
+    sig2 = 1/(sigma*sigma);
+    for (person=0; person<n; person++) {
+	z = (time[person] - eta[person]) /sigma;
+
+	j = status[person];       /*convert to integer */
+	switch(j) {
+	    case 1:                             /* exact */
+		(*sreg_gg)(z, funs,1);
+		g[person] = log(funs[1])  - log(sigma);
+		temp = funs[2]/sigma;
+		dg[person] = -temp;
+		ddg[person]= funs[3]*sig2 - temp*temp;
+		break;
+	    case 0:                             /* right censored */
+		(*sreg_gg)(z, funs,2);
+		g[person] = log(funs[1]);
+		temp = funs[2]/(funs[1]*sigma);
+		dg[person] = temp;
+		ddg[person]= -funs[3]*sig2/funs[1] - temp*temp;
+		break;
+	    case 2:                             /* left censored */
+		(*sreg_gg)(z, funs,2);
+		g[person] = log(funs[0]);
+		temp = funs[2]/(funs[0]*sigma);
+		dg[person]= -temp;
+		ddg[person]= funs[3]*sig2/funs[0] - temp*temp;
+		break;
+	    case 3:                             /* interval censored */
+		zu = (time2[person] - eta[person])/sigma;  /*upper endpoint */
+		(*sreg_gg)(z, funs, 2);
+		(*sreg_gg)(zu,ufun ,2);
+		if (z>0)  temp = funs[1] - ufun[1]; /*stop roundoff in tails*/
+		else      temp = ufun[0] - funs[0];
+		g[person] = log(temp);
+		dg[person]  = -(ufun[2] - funs[2])/(temp*sigma);
+		ddg[person] = (ufun[3] - funs[3])*sig2/temp -
+						     dg[person]*dg[person];
+		if (pfixed[0]==0) {
+		    dsig[person] = (z*funs[2] - zu*ufun[2])/temp;
+		    ddsig[person]= ((zu*zu*ufun[3] - z*z*funs[3])
+				      +(zu*ufun[2] - z*funs[2])) /temp -
+				    dsig[person]*dsig[person];
+		    dsg[person] = ((zu*ufun[3] - z*funs[3])
+				   + (ufun[2] - funs[2])) /(temp*sigma)  -
+				      dsig[person]*dg[person];
+		    }
+		break;
+	    }
+	}
+    }
 
 /*
 **  Case      ans[0]    ans[1]       ans[2]     ans[3]
