7ec1e37717a3949021f4036214a275116364ab2a
splus <unknown@noreply.com>
1992-11-18 15:07:22 +0000

    Fix error in symbol names
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9898 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survival_i.c
+++ trunk/survival/trunk/survival_i.c
@@ -1,3 +1,4 @@
+/* SCCS $Id: survival_i.c,v 4.2 1992-11-18 15:07:22 splus Exp $  */
 #include "S.h"
 #include "cdefs.h"
 #if defined(IRIS4D) || defined(DECSTATION) /* ( */
@@ -9,15 +10,15 @@ extern void agexact();
 extern void agfit2();
 extern void agfit_null();
 extern void aghaz2();
-extern void agres1();
-extern void agres2();
+extern void agres12();
+extern void agres22();
 extern void agsurv1();
 extern void agsurv2();
 extern void coxfit2();
 extern void coxfit_null();
 extern void coxhaz2();
-extern void coxres1();
-extern void coxres2();
+extern void coxres12();
+extern void coxres22();
 extern void survdiff2();
 extern void survexp2();
 extern void survfit2();
@@ -29,15 +30,15 @@ x_h survival_init[]  = {
 {SYMBOL(agfit2),(long)agfit2,NULL},
 {SYMBOL(agfit_null),(long)agfit_null,NULL},
 {SYMBOL(aghaz2),(long)aghaz2,NULL},
-{SYMBOL(agres1),(long)aghaz2,NULL},
-{SYMBOL(agres2),(long)aghaz2,NULL},
+{SYMBOL(agres12),(long)aghaz2,NULL},
+{SYMBOL(agres22),(long)aghaz2,NULL},
 {SYMBOL(agsurv1),(long)agsurv1,NULL},
 {SYMBOL(agsurv2),(long)agsurv2,NULL},
 {SYMBOL(coxfit2),(long)coxfit2,NULL},
 {SYMBOL(coxfit_null),(long)coxfit_null,NULL},
 {SYMBOL(coxhaz2),(long)coxhaz2,NULL},
-{SYMBOL(coxres1),(long)coxhaz2,NULL},
-{SYMBOL(coxres2),(long)coxhaz2,NULL},
+{SYMBOL(coxres12),(long)coxhaz2,NULL},
+{SYMBOL(coxres22),(long)coxhaz2,NULL},
 {SYMBOL(survdiff2),(long)survdiff2,NULL},
 {SYMBOL(survexp2),(long)survexp2,NULL},
 {SYMBOL(survfit2),(long)survfit2,NULL},
