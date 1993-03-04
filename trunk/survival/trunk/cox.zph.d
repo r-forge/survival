48194d0e16ab1c3b48793afa4e507c6020f5a0c8
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1993-03-04 12:01:11 +0000

    Track new version of cox.zph
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9940 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/cox.zph.d
+++ trunk/survival/trunk/cox.zph.d
@@ -3,27 +3,46 @@
 .TL
 Test the proportional hazards assumption
 .CS
-cox.zph(fit, transform='rank', global=F)
+cox.zph(fit, transform='km', global=T, x=F)
 .AG fit
 the result of fitting a Cox regression model, using the coxph function.
 .AG transform
 survival times transformed before the test is performed.  Possible values
-are the character strings 'rank', 'identity', or a function of one argument.
+are the character strings 'km', 'rank', 'identity', or a function of one argument.
 .AG global
-either a global chisquare test, or per-variable tests will be done.
+should a global chisquare test be done, in additoin to the
+ per-variable tests.
+.AG x
+if true, then the result will be a list containing the test table (a matrix),
+x and y.  If false then only the test table is returned.
 .RT
-a matrix with one row for each variable or a vector if the
-global test.
+.AG table
+a matrix with one row for each variable, and optionally a last row for
+the global test.
 Columns of the matrix contain the correlation coefficient between transformed
 survival time and the scaled Schoenfeld residuals, a chisquare,
 and the two-sided p value.
+For the global test there is no appropriate correlation, so an NA is
+entered into the matrix as a placeholder.
+.AG x
+the transformed time axis.
+.AG y
+the matrix of scaled Schoenfeld residuals.  There will be one column per
+variable and one row per event.  The row labels contain the original event
+times, which will be the same as `x' for the identity transform.
 .pp
+The computations require the original `x' matrix of the Cox model fit.
+Thus it saves time if the `x=T' option is used in coxph.
+
 This function would usually be executed in conjunction with a plot of the
-scaled Schoenfeld residuals versus survival time.
+scaled Schoenfeld residuals versus survival time.  The plot gives an
+estimate of the time-dependent coefficient beta(t).
 .SH REFERENCES
 Terry Therneau, author of local function.
 .EX
-fit <- coxph( Surv(futime, fustat) ~ age + surgery, jasa)
-cox.zph(fit)
+fit <- coxph( Surv(futime, fustat) ~ age + surgery, jasa, x=T)
+temp<- cox.zph(fit, x=T)
+print(temp)                  #display the results
+plot(temp)                   #plot curves
 .KW survival
 .WR
