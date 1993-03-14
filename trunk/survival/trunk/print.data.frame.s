ce98279d36be3c72d90b3064409d8f3ac4f6e004
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1993-03-14 19:32:27 +0000

    Better support for Surv and date objects.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9944 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/print.data.frame.s
+++ trunk/survival/trunk/print.data.frame.s
@@ -1,4 +1,4 @@
-#SCCS $Date: 1992-04-14 18:06:06 $ $Id: print.data.frame.s,v 4.2 1992-04-14 18:06:06 grill Exp $
+#SCCS $Date: 1993-03-14 19:32:27 $ $Id: print.data.frame.s,v 4.3 1993-03-14 19:32:27 therneau Exp $
 #
 # In order to get objects with attributes to print correctly, I replace the
 #   call to "as.matrix" with a copy of as.matrix.data.frame, one that knows
@@ -26,16 +26,17 @@ function(x, ..., quote = F, right = T)
 	    xj <- x[[j]]
 
 	    # 4 Line below are the ones I added
-	    if (inherits(xj, "data.frame"))
-		xj <- x[[j]] <- as.matrix(x[[j]])
-	    else if (!is.null(class(xj)))
-		xj <- x[[j]] <- as.character(x[[j]])
+	    if (!is.null(cl <-class(xj)) &&
+		       (cl == 'Surv' || cl=='date'))
+			     xj <- x[[j]] <- as.character(x[[j]])
 	    if(length(dj <- dim(xj)) == 2 && dj[2] > 1) {
+		if (inherits(xj, "data.frame"))
+		    xj <- x[[j]] <- as.matrix(x[[j]])
 		dnj <- dimnames(xj)[[2]]
 		collabs[[j]] <- paste(collabs[[j]], if(length(dnj)) dnj
 		     else seq(1:dj[2]), sep = ".")
 		}
-	    if(length(levels(xj)) > 0 || !is.numeric(xj) )
+	    if(length(levels(xj)) > 0 || (!is.complex(xj) ||!is.numeric(xj)))
 		non.numeric <- TRUE
 	    if(!is.atomic(xj))
 		non.atomic <- TRUE
@@ -51,15 +52,15 @@ function(x, ..., quote = F, right = T)
 	else if(non.numeric) {
 	    for(j in 1:p) {
 		xj <- x[[j]]
-		if(length(ll <- levels(xj)))
-		    x[[j]] <- ll[unclass(xj)]
+		if(length(levels(xj)))
+		    x[[j]] <- as.vector(xj)
 		else x[[j]] <- format(xj)
 		}
 	    }
 	x <- unlist(x, recursive = F)
 	dim(x) <- c(n, length(x)/n)
-	dimnames(x) <- list(dn[[1]], unlist(collabs))
+	dimnames(x) <- list(dn[[1]], unlist(collabs, use.names=F))
 	class(x) <- "matrix"
-	print(x, quote=quote, right=right)
+	print(x, ..., quote=quote, right=right)
 	}
     }
