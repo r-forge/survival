f32ceed97383fb3d412beea013e7e5fe087c4498
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1993-06-10 16:51:56 +0000

    Add "weights" argument.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@9991 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/model.frame.coxph.s
+++ trunk/survival/trunk/model.frame.coxph.s
@@ -1,7 +1,7 @@
 model.frame.coxph <- function(object, ...) {
     Call <- object$call
     Call[[1]] <- as.name("model.frame")
-    Call <- Call[match(c("", "formula", "data", "subset", "na.action"),
-		     names(Call), 0)]
+    Call <- Call[match(c("", "formula", "data", "weights", "subset",
+			   "na.action"), names(Call), 0)]
     eval(Call)
     }
