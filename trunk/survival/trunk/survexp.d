0db0d297b1d2418b983f3ca33c74091b29e9f6ff
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1993-12-02 21:37:59 +0000

    Complete rewrite, along with person-years
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10011 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survexp.d
+++ trunk/survival/trunk/survexp.d
@@ -1,112 +1,97 @@
 .BG
 .FN survexp
 .TL
-Compute expected survival
+Compute Expected Survival
 .DN
-Computes the expected survival post study entry for a cohort of subjects,
-based on their age and sex composition.
+Returns either the expected survival of a cohort of subjects, or the
+individual expected survival for each subject.
 .CS
-survexp(entry, birth, sex,
-data, subset, na.action,
-times=round(182.6 * 0:8),
-type=c("mean", "individual", "matrix"), expected=survexp.uswhite, interp=F)
+survexp(formula, data, weights, subset, na.action,
+ times, cohort=T, ratetable=survexp.uswhite, scale=365.25, model=F, x=F, y=F)
 .RA
-.AG entry
-date of entry to the study.  Must be a date value as returned by mdy.date.
-.AG birth
-date of birth, a date value as returned by mdy.date.  Optionally, this may
-contain the patient's age, in which case the date of birth is inferred by
-subtracting round(365.25*age) from the entry date.
-.AG sex
-this indexes the second dimension of the expected array. For the example
-provided-- US white-- the values are 1=male, 2=female.
+.AG formula
+a formula object.  The response variable will be a vector of follow-up times,
+and is optional.  The predictors will consist of optional grouping variables
+separated by + operators (exactly as in `survfit'), along with a
+`ratetable()' term.  This latter matches each subject to his/her expected
+cohort.
 .OA
-.AG data
-frame in which to find the variables.
-.AG subset, na.action
-the subset of observations to be used in the calculation, and the function
-to be used to handle any NAs in the data.
+.AG data, weights, subset, na.action
+as in other modeling routines.
+Weights are currently ignored.
 .AG times
-vector of time points at which an expected survival is desired.  If survexp
-is used for plotting purposes, then this can be a fairly coarse grid, since
-the expected survival function is very smooth.
-.AG type
-if 'mean', then the average expected survival for the cohort is returned.
-The result will often be added to an existing Kaplan-Meier plot.
-If type=='individual', then the times vector must be of the same length as
-entry and birth date, and the return value contains the expected survival
-of each individual at his/her respective time.
-This form is most often used with the one sample log-rank test, see survdiff.
-If type=='matrix', then one column is returned per subject.
-.AG expected
-a 2 or 3 dimensional array, age by sex by calendar year, containing the
-expected survival data.
-The second dimension can be of any size >1.  For instance, a table containing
-both sex and 4 catorizations of race would have second dimension 8.
-The `sex' argument is understood to be an index into this dimension of the
-table.
-.AG interp
-if T, then the `expected' matrix is filled out to contain one entry per year,
-by linear interpolation.
+an optional vector of times at which the resulting survival curve should
+be evaluated.  If absent, the result will be reported for each unique value
+of the vector of follow-up times.
+.AG cohort
+If false, each subject is treated as a subgroup of size 1.
+.AG conditional
+If `y' is missing in the formula, this argument is ignored.  Otherwise it
+is an indicator of whether y includes death times, which leads to conditional
+expected survival, or includes only the potential censoring times.
+.AG ratetable
+a table of event rates, such as survexp.uswhite.
+.AG scale
+a scaling for the results.  As most rate tables are in units/day, the
+default value of 365.25 causes the output to be reported in years.
+.AG model, x, y
+flags to control what is returned.  If any of these is true, then the
+model frame, the model matrix, and/or the vector of response times will be
+returned as components of the final result, with the same names as the
+flag arguments.
 .RT
-a structure with components
-.AG  time
-vector of time values
-.AG surv
-if mean=T, a vector of survival probablities for the cohort as a whole.  If
-mean=F and length(times) = number of subjects, then y is a vector of
-survival probabilities for each subject at that subject's time value.
-Otherwise y will be a matrix containing each subject's expected survival at
-each time in the times vector.
-.AG n
-number of subjects, after removal of missing values.  An attribute 'omit'
-contains the indices of the removed observations, if any.
-.SH METHOD
-The expected matrix is constructed from national life tables; a
-sample of references is below.
-We make use of the life table `q' for each year of age, which is the
-probability that a subject who reaches that age will die before reaching
-age+1.  For example, in the 1980 table for white males, the row for age
-62 is the probability that someone who becomes 62 in 1980 will die before his
-63rd birthday.
-.pp
-To make interpolation easier, the expected table used by the program contains
-the per day hazard, i.e., -log(1-q) /365.25.  For a 62 year old male in 1980
-q=.0212 and h= .0004868.  The cumulative hazard over an
-interval is then just #days * hazard.
-For each subject, a cumulative hazard
-function is constructed which is piecewise linear in intervals starting at
-their birth date.  The values at the desired times from entry to the study
-are then plucked off of this curve, and the survival at those times
-is computed as exp(-cum hazard).  The overall survival is the mean of the
-individual patient survivals.
-.pp
-The dimnames of the hazard array `expected' control which entries of the
-array are used.  The first name vector contains the starting age for each
-age interval.  The first interval must start at 0, but intervals need not be
-sequential integers as used in the default survexp.uswhite data set, e.g.,
-a study of infant mortality would likely wish to divide the first year more
-finely, or a non-US population might have data only on 5 year age groups.
-(The internal calculations are done in days, so there is a lower limit to the
-granularity.)  The third subscript contains the starting year.  For instance,
-if 60-65 were an age interval, then the calendar year in which the subject
-turns 60 will be used in choosing the appropriate hazard rate from the array.
-This hazard rate is applied for the entire age interval.
-.pp
-The last age and the last calendar year of the array are both considered to
-be open ended, i.e., they apply to all later ages and calendar years,
-respectively.  The first calendar year is applied for all earlier years
-(in defiance of it's labeled starting date).
+if cohort=T an object of class `survexp', otherwise a vector of per-subject
+expected survival values.  The former contains the number of subjects at
+risk and the expected survival for the cohort at each requested time.
+.DT
+Individual expected survival is ususally used in models or testing, to
+`correct' for the age and sex composition of a group of subjects.  For
+instance, assume that birth date, entry date onto the study,sex and
+actual survival time are all known for a group of subjects.
+The uswhite population tables contain expected death rates
+based on calendar year, sex and age.  Then
+.Cs
+dprob <- 1 -survexp(death.time ~ ratetable(sex=sex, year=entry.dt, age=(birth.dt-entry.dt)), cohort=F)
+.Ce
+gives for each subject the probability of death at or before their observed
+death time.  This probability can be used as a rescaled time value in
+models:
+.Cs
+glm(status ~ 1 + offset(log(dprob)), family=poisson)
+glm(status ~ x + offset(log(dprob)), family=poisson)
+.Ce
+In the first model, a test for intercept=0 is the one sample log-rank
+test of whether the observed group of subjects has equivalent survival to
+the baseline population.  The second model tests for an effect of variable
+`x' after adjustment for age and sex.
+.PP
+Cohort survival is used to produce an overall survival curve.  This is then
+added to the Kaplan-Meier plot of the study group for visual comparison
+between these subjects and the population at large.  There are three common
+methods of computing cohort survival.
+In the "exact method" of Ederer the cohort is not censored; this corresponds
+to having no response variable in the formula.  Hakulinen recommends censoring
+the cohort at the anticipated censoring time of each patient, and Verhuel
+recommends censoring the cohort at the actual observation time of each
+patient.  These are obtained by using the respective time values as the
+follow-up time or response in the formula.
 .SH REFERENCES
-National Office of Vital Statistics: Life Tables for the Geographic Divisions
-of the United States: 1959-61.  Vol. 1, No. 3.  Public Health Service,
-Washington.  U.S. Government Printing Office, May 1965.
+G. Berry.  The analysis of mortality by the subject-years method.
+Biometrics 1983, 39:173-84.
+.br
+F Ederer, L Axtell, and S Cutler.  The relative survival rate: a statistical
+methodology. Natl Cnacer Inst Monogr 1961, 6:101-21.
+.br
+T. Hakulinen.  Cancer survival corrected for heterogeneity in patient
+withdrawal.  Biometrics 1892, 38:933.
+.br
+H. Verheul, E. Dekker, P. Bossuyt, A. Moulijn, and A. Dunning.  Backround
+mortality in clinical survival studies.  Lancet 1993, 341:872-5.
 .SA
-survfit, survdiff
+survfit, survexp.uswhite, survexp.fit, personyr, date
 .EX
-xx <-  survfit(last.dt - entry.dt, last.status)
-plot(xx)
-maxt <- max(xx$time)
-lines(survexp(entry.dt, birth.dt, sex, times=seq(0,maxt,length=10), lty=2)
+efit <- survexp( ~ ratetable(sex=sex, year=entry.dt, age=entry.dt-birth.dt))
+plot(survfit(Surv(futime, status) ~1))
+lines(efit)
 .KW survival
 .WR
