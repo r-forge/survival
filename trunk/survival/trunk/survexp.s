49b0555c8ce3977bcfdd2020cb72c39391ebc84d
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1993-12-02 21:30:52 +0000

    Complete rewrite, along with person-years package
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10010 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survexp.s
+++ trunk/survival/trunk/survexp.s
@@ -1,121 +1,129 @@
-#SCCS $Id: survexp.s,v 4.12 1992-08-11 08:15:40 grill Exp $
-survexp <- function(entry, birth, sex,
-		      times=round(182.6 * 0:8),
-		      data=sys.parent(), subset, na.action,
-		      type=c("mean", "individual", "matrix"),
-		      expected=survexp.uswhite, interp=F) {
+#SCCS  $Id: survexp.s,v 4.13 1993-12-02 21:30:52 therneau Exp $
+survexp <- function(formula=formula(data), data=sys.parent(),
+	weights, subset, na.action,
+	times,  cohort=T,  conditional=T,
+	ratetable=survexp.uswhite, scale=365.25,
+	model=F, x=F, y=F) {
+
     call <- match.call()
+    m <- match.call(expand=F)
+    m$ratetable <- m$model <- m$x <- m$y <- m$scale<- m$cohort <- NULL
+    m$times <- m$conditional <- NULL
 
-    type <- match.arg(type)
-
-    m <- call
-    if (type=='individual') {
-	if (missing(times))
-	    stop ("The 'times' argument is required for individual survival")
-	temp <- c("entry", "birth", "sex", "times")
-	}
-    else {
-	temp <- c("entry", "birth", "sex")
-	if (any(is.na(times))) stop("Missing values not allowed in 'times'")
-	}
-    temp2 <- NULL
-    for (i in temp)
-	 if (mode(call[[i]]) =='numeric')
-		temp2 <- c(temp2, paste("I(", deparse(call[[i]]), ")"))
-	 else   temp2 <- c(temp2, deparse(call[[i]]))
-    m$formula <- formula(paste("~", paste(temp2, collapse="+")))
+    Terms <- if(missing(data)) terms(formula, 'ratetable')
+	     else              terms(formula, 'ratetable',data=data)
+    if (any(attr(Terms, 'order') >1))
+	    stop("Survexp cannot have interaction terms")
+    m$formula <- Terms
     m[[1]] <- as.name("model.frame")
-    m <- m[match(c("", "formula", "subset", "na.action", "data"),
-		     names(m), 0)]
     m <- eval(m, sys.parent())
+    n <- nrow(m)
 
-    entry <- m[[1]]
-    birth <- m[[2]]
-    sex   <- m[[3]]
-    if (type=='individual') times <- m[[4]]
-    else    times <- as.integer(sort(unique(times)))
-    n <- length(entry)
-    if (!inherits(entry, "date")) stop ("'Entry' must be a date")
-    if (!inherits(birth, "date")) {
-	# Assume that they gave an age
-	if (any(birth <=0)) stop ("Age must be >0 ")
-	if (any(birth >130))stop ("Impossibly large value for age at entry")
-	birth <- entry - round(birth*365.2425)
-	}
-
-    if (any(entry<birth)) stop("Subject entered before birth")
-    if (any(times<0)) stop ("'Times' must be >=0")
-    ntime <- length(times)
-
-    dn <- dimnames(expected)
-    dd <- dim(expected)
-    if (is.null(dd) || length(dd) <2 || dd[2]<2 || length(dd) >3 )
-	stop("The hazard table is not in the correct format")
-    ages <- as.integer(round(365.2425 *as.numeric(dn[[1]])))
-    if (ages[1] !=0  ||  length(ages)<2 || any(diff(ages)<=0))
-	stop("The hazard table is not in the correct format")
-
-    if (!all(sex==floor(sex) & sex>0 & sex<=dd[2]))
-	 stop("Invalid value for \'sex\'")
-
-    if (length(dd) ==2 || dd[3]==1) {
-	years <- 1
-	dd[3] <- 1
+    Y <- model.extract(m, 'response')
+    no.Y <- is.null(Y)
+    if (!no.Y) {
+	if (is.matrix(Y) && ncol(Y)>1) {
+	    if (cohort || ncol(Y)!=2)
+		stop("Illegal response value, for cohort survival")
+	    if (Y[,1] >= Y[,2]) stop("Stop time must be > start time")
+	    if (Y[,1] <0) stop("Start time must be >=0")
+	    }
+	else if (any(Y <0)) stop ("Negative follow up time")
 	}
     else {
-	years <- as.integer(dn[[3]])
-	if (any(diff(years)<=0)) stop ("The hazard table must have increasing `years'")
-	if (!interp || all(diff(years)==1)) years <- mdy.date(1,1, years)
-	else { # linearly interpolate the rows
-	    nyear <- 1+max(years) - min(years)
-	    temp <- matrix(double(dd[3] * nyear), ncol=nyear)
-	    diffs <- 1/diff(years)
-	    new <- (min(years)):(max(years))
-	    for (i in 1:(dd[3]-1)) {
-		xx <- 1 - (new - years[i])*diffs[i]
-		xx <- ifelse(xx<0 | xx>1, 0, xx)
-		temp[i,] <- temp[i,] + xx
-		temp[i+1,] <- temp[i+1,] + ifelse(xx==0, 0, 1-xx)
+	conditional <- F
+	if (missing(times))
+	   stop("There is no times argument, and no follow-up times are given in the formula")
+	Y <- rep(max(times), n)
+	}
+    if (!missing(times)) newtime <- sort(unique(c(Y, times)))
+    else                 newtime <- sort(unique(Y))
+    weights <- model.extract(m, 'weights')
+
+    rate <- attr(Terms, "specials")$ratetable
+    if (length(rate)==0)
+	stop("Must have a ratetable() call in the formula")
+    if (length(rate) >1 )
+	stop ("Can have only 1 ratetable() call in a formula")
+
+    ovars <- (dimnames(attr(Terms, 'factors'))[[1]])[-c(1, rate)]
+    temp <- match.ratetable(m[,rate], ratetable)
+    R <- temp$R
+    if (!is.null(temp$call)) {  #need to dop some dimensions from ratetable
+	ratetable <- eval(parse(text=temp$call))
+	}
+
+    if (cohort) {
+	# Now process the other (non-ratetable) variables
+	if (length(ovars)==0)  X <- rep(1,n)  #no categories
+	else {
+	    odim <- length(ovars)
+	    for (i in 1:odim) {
+		temp <- m[[ovars[i]]]
+		ctemp <- class(temp)
+		if (!is.null(ctemp) && ctemp=='tcut')
+		    stop("Can't use tcut variables in expected survival")
 		}
-	    temp[dd[3], nyear] <- 1
-	    expected <- matrix(expected,ncol=dd[3]) %*% temp
-	    dd[3] <- nyear
-	    dim(expected) <- dd
-	    years <- mdy.date(1,1,new)
+	    X <- strata(m[ovars])
 	    }
-	}
-    if (type != 'matrix') nsurv <- 1
-    else                  nsurv <- n
-    temp <-  .C("survexp2", as.integer(ntime),
-			    as.integer(times),
-			    as.integer(dd),
-			    ages,
-			    as.integer(years),
-			    as.single(expected),
-			    as.integer(n),
-			    as.integer(entry),
-			    as.integer(birth),
-			    as.integer(sex),
-			    double(ntime),
-			    as.integer(nsurv),
-			    as.integer(type=='individual'),
-			    surv= double(ntime*nsurv))
+	temp <- survexp.fit(cbind(as.numeric(X),R), Y, newtime,
+			       conditional, ratetable)
 
-    if (type != 'matrix') xx <- list(time=times, surv=temp$surv, n=n)
-    else     xx <-  list(time=times,
-			 surv=matrix(temp$surv, ncol=n, byrow=F),
-			 n=n)
-    xx$call <- call
-    if(!is.null(tj <- attr(m, 'na.action')))
-	xx$na.action <- tj
-    if (type == 'individual') {
-	attr(xx, "class") <- "survexp"
-	if (!is.null(tj)) {
-	    xx$time <- naresid(tj, times)
-	    xx$surv <- naresid(ty, xx$surv)
+	if (missing(times)) {
+	    n.risk <- temp$n
+	    if (is.matrix(temp$surv)) surv <- apply(temp$surv, 2,cumprod)
+	    else                      surv <- cumprod(temp$surv)
 	    }
+	else {
+	    keep <- match(times, newtime)
+	    if (is.matrix(temp$surv)) {
+		surv <- apply(temp$surv, 2, cumprod)
+		surv <- surv[keep,]
+		n.risk <- temp$n[keep,]
+		}
+	    else {
+		surv <- cumprod(temp$surv)
+		surv <- surv[keep]
+		n.risk <- temp$n[keep]
+		}
+	    newtime <- times
+	    }
+	newtime <- newtime/scale
+	if (length(ovars)) {    #matrix output
+	    if (no.Y) { #use matrix formulation
+		out <- list(call=call, surv=surv, n.risk=c(n.risk[,1]),
+			    time=newtime)
+		}
+	    else {
+		#I can't use the "matrix" form of surv, due to different n's
+		out <- list(call=call, surv=c(surv), n.risk=c(n.risk),
+				time = rep(newtime, ncol(surv)))
+		tstrat <- rep(nrow(surv), ncol(surv))
+		names(tstrat) <- levels(X)
+		out$strata <- tstrat
+		}
+	    }
+	else  out <- list(call=call, surv=c(surv), n.risk=c(n.risk),
+			   time=newtime)
+
+	na.action <- attr(m, "na.action")
+	if (length(na.action))  out$na.action <- na.action
+	if (model) out$model <- m
+	else {
+	    if (x) out$x <- structure(cbind(X, R),
+		dimnames=list(row.names(m), c("group", dimid)))
+	    if (y) out$y <- Y
+	    }
+	class(out) <- c("survexp", "survfit")
+	out
 	}
-    else attr(xx, "class") <- c("survexp", "survfit")
-    xx
+
+    else { #individual survival
+	temp <- survexp.fit(cbind(1:n,R), Y, max(Y), conditional, ratetable)
+	xx <- temp$surv
+	names(xx) <- row.names(m)
+	na.action <- attr(m, "na.action")
+	if (length(na.action)) naresid(na.action, xx)
+	else xx
+	}
     }
-
