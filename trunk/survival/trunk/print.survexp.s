308b4ba012cc35ada9bd90053fc2294c1fa4d58d
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1993-12-02 22:07:00 +0000

    total rewrite, for new version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10017 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/print.survexp.s
+++ trunk/survival/trunk/print.survexp.s
@@ -1,22 +1,47 @@
-#SCCS $Date: 1992-04-15 10:21:35 $ $Id: print.survexp.s,v 4.4 1992-04-15 10:21:35 therneau Exp $
-print.survexp <- function(object, ...) {
-    if (!is.null(cl<- object$call)) {
-	cat("Call:  ")
+#SCCS $Id: print.survexp.s,v 4.5 1993-12-02 22:07:00 therneau Exp $
+print.survexp <- function(fit, scale=1, digits=3, ...) {
+    if (!inherits(fit, 'survexp'))
+	    stop("Invalid data")
+    savedig <- options(digits=digits)
+    on.exit(options(savedig))
+
+    if (!is.null(cl<- fit$call)) {
+	cat("Call: ")
 	dput(cl)
 	cat("\n")
 	}
 
-    omit <- object$na.action
+    omit <- fit$na.action
     if (length(omit))
-	cat("  n=", object$n, " (", naprint(omit), ")\n", sep="")
-    else cat("  n=", object$n, "\n")
+	cat(naprint(omit), "\n")
 
-    temp <- as.matrix(object$surv)
-    if (ncol(temp) ==1)
-	dimnames(temp) <- list(object$time, "Expected")
-    else
-	dimnames(temp) <- list(object$time,
-			       paste("Expected", seq(ncol(temp)), sep=''))
-    print.matrix(temp)
-    invisible(temp)
+    mat <- cbind(fit$time, fit$n.risk, fit$surv)
+    cnames <- c("time", "n.risk")
+    if (is.matrix(fit$surv)) {
+	ncurve <- ncol(fit$surv)
+	cnames <- c(cnames, paste("survival", seq(ncurve), sep=''))
+	}
+    else {
+	ncurve <- 1
+	cnames <- c(cnames, "survival")
+	}
+
+    dimnames(mat) <- list(NULL, cnames)
+    if (is.null(fit$strata)) {
+	prmatrix(mat, rowlab=rep("", nrow(mat)))
+	}
+    else  { #print it out one strata at a time
+	nstrat <- length(fit$strata)
+	stemp <- rep(1:nstrat,fit$strata)
+	strata <- factor(stemp,
+	    labels = names(fit$strata)[sort(unique(stemp))])
+	for (i in levels(strata)) {
+	    who <- (strata==i)
+	    cat("               ", i, "\n")
+	    if (sum(who) ==1) print(mat[who,])
+	    else    prmatrix(mat[who,], rowlab=rep("", sum(who)))
+	    cat("\n")
+	    }
+	}
+    invisible(fit)
     }
