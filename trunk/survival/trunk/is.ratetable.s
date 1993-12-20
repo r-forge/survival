be4d225c7164fda23b18b16b3db0c130b5252a70
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1993-12-20 09:21:13 +0000

    Initial revision
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10025 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/survival/trunk/is.ratetable.s
@@ -0,0 +1,22 @@
+#SCCS $Id: is.ratetable.s,v 4.1 1993-12-20 09:21:13 therneau Exp $
+is.ratetable <- function(x) {
+    if (!inherits(x, 'ratetable')) return(F)
+    att <- attributes(x)
+    if (any(is.na(match(c("dim", "dimnames", "dimid", "factor", "cutpoints"),
+			names(att))))) return(F)
+    nd <- length(att$dim)
+    if (length(x) != prod(att$dim)) return(F)
+    if (!(is.list(att$dimnames) && is.list(att$cutpoints))) return(F)
+    if (length(att$dimnames)!=nd || length(att$factor)!=nd ||
+		     length(att$cutpoints)!=nd) return(F)
+    fac <- as.numeric(att$factor)
+    if (any(is.na(fac))) return(F)
+    if (any(fac <0)) return(F)
+    for (i in 1:nd) {
+	n <- att$dim[i]
+	if (length(att$dimnames[[i]]) !=n) return(F)
+	if (fac[i]!=1 && length(att$cutpoints[[i]])!=n) return(F)
+	if (fac[i]==1 && !is.null(att$cutpoints[[i]]))  return(F)
+	}
+    T
+    }
