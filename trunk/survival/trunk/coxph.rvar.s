ee6c833a47d48e308cf81d5bbe0fcadd67642ff6
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1994-01-17 09:36:32 +0000

    change resid call from 'dbeta' to 'dfbeta'
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10039 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/coxph.rvar.s
+++ trunk/survival/trunk/coxph.rvar.s
@@ -1,10 +1,10 @@
-#SCCS $Id: coxph.rvar.s,v 4.1 1993-07-04 16:08:15 therneau Exp $
+#SCCS $Id: coxph.rvar.s,v 4.2 1994-01-17 09:36:32 therneau Exp $
 coxph.rvar <- function(fit, collapse) {
     rcall <- match.call()
     if (class(fit) != 'coxph')
 	stop ("First argument must be a fitted Cox model")
 
-    if (missing(collapse)) temp <- residuals.coxph(fit, type='dbeta')
+    if (missing(collapse)) temp <- residuals.coxph(fit, type='dfbeta')
     else temp <- residuals.coxph(fit, type='dbeta', collapse=collapse)
 
     fit$robust.var <- t(temp) %*% temp
