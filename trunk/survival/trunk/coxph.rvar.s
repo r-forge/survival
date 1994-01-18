c38cd7cf4af4ac97c1c630954ab5c013a7771051
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1994-01-18 10:30:48 +0000

    Didn't handle missings!
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10040 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/coxph.rvar.s
+++ trunk/survival/trunk/coxph.rvar.s
@@ -1,12 +1,14 @@
-#SCCS $Id: coxph.rvar.s,v 4.2 1994-01-17 09:36:32 therneau Exp $
+#SCCS $Id: coxph.rvar.s,v 4.3 1994-01-18 10:30:48 therneau Exp $
 coxph.rvar <- function(fit, collapse) {
     rcall <- match.call()
     if (class(fit) != 'coxph')
 	stop ("First argument must be a fitted Cox model")
 
     if (missing(collapse)) temp <- residuals.coxph(fit, type='dfbeta')
-    else temp <- residuals.coxph(fit, type='dbeta', collapse=collapse)
-
+    else temp <- residuals.coxph(fit, type='dfbeta', collapse=collapse)
+    if (any(is.na(temp)))
+       if (ncol(temp)==1) temp<- temp[!is.na(temp),,drop=F]
+       else               temp <- temp[!is.na(temp %*% rep(1,ncol(temp))),]
     fit$robust.var <- t(temp) %*% temp
     fit$rcall <- rcall
     fit
