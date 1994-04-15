e04c78ba0d7cabc43e3e44092ea46411c259b7f8
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1994-04-15 09:25:52 +0000

    Fix a conflict between the strata arg and the strata() function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10067 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/coxph.getdata.s
+++ trunk/survival/trunk/coxph.getdata.s
@@ -1,20 +1,25 @@
-# SCCS $Id: coxph.getdata.s,v 4.1 1994-04-08 15:23:38 therneau Exp $
+# SCCS $Id: coxph.getdata.s,v 4.2 1994-04-15 09:25:52 therneau Exp $
 #
 # Reconstruct the Cox model data.  This is done in so many routines
 #  that I extracted it out.
 #
-coxph.getdata <- function(fit, y=T, x=T, strata=T, offset=F) {
+# The "stratax" name is to avoid conflicts with the strata() function, but
+#   still allow users to type "strata" as an arg.
+#
+coxph.getdata <- function(fit, y=T, x=T, stratax=T, offset=F) {
     ty <- fit$y
     tx <- fit$x
     strat <- fit$strata
     Terms <- fit$terms
+    if (is.null(attr(Terms, 'offset'))) offset <- F
+    if (offset) x<- T
     if (!inherits(Terms, 'terms'))
 	    stop("invalid terms component of fit")
     strats <- attr(Terms, "specials")$strata
+    if (length(strats)==0) stratax <- F
 
     if ( (y && is.null(ty)) || (x && is.null(tx)) ||
-	     (strata && length(strats) && is.null(strat)) ||
-		(offset && is.null(offset))) {
+	     (stratax && is.null(strat)) || offset) {
 	# get the model frame
 	m <- fit$model
 	if (is.null(m)) m <- model.frame(fit)
@@ -22,25 +27,26 @@ coxph.getdata <- function(fit, y=T, x=T, strata=T, offset=F) {
 	# Pull things out
 	if (y && is.null(ty)) ty <- model.extract(m, 'response')
 
-	if (offset) toff <- model.extract(m, 'response')
+	if (offset) toff <- model.extract(m, 'offset')
 
 	# strata was saved in the fit if and only if x was
 	if (x && is.null(tx)) {
-	    if (length(strats)) {
+	    if (stratax) {
 		temp <- untangle.specials(Terms, 'strata', 1)
 		tx <- model.matrix(Terms[-temp$terms], m)[,-1,drop=F]
-		if (strata)
-		    strat <- (get("strata",mode="function"))(m[temp$vars], shortlabel=T)
+		strat <- strata(m[temp$vars], shortlabel=T)
 		}
 	    else tx <- model.matrix(Terms, m)[,-1,drop=F]   #remove column of 1's though
 	    }
 	}
+    else if (offset)
+       toff <- fit$linear.predictors -(c(tx %*% fit$coef) - sum(fit$means*fit$coef))
 
     temp <- NULL
     if (y) temp <- c(temp, "y=ty")
     if (x) temp <- c(temp, "x=tx")
-    if (strata && !is.null(strat)) temp <- c(temp, "strata=strat")
-    if (offset && !is.null(toff))  temp <- c(temp, "offset=toff")
+    if (stratax)  temp <- c(temp, "strata=strat")
+    if (offset)  temp <- c(temp, "offset=toff")
 
     eval(parse(text=paste("list(", paste(temp, collapse=','), ")")))
     }
