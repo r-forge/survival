fb61145d1cf4c12511d268e0b881601af8fa3a0e
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1996-04-09 07:49:47 +0000

    Added "verbose" option
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10158 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/is.ratetable.s
+++ trunk/survival/trunk/is.ratetable.s
@@ -1,24 +1,29 @@
-#SCCS $Id: is.ratetable.s,v 4.3 1995-10-27 12:17:28 therneau Exp $
-is.ratetable <- function(x) {
-    if (!inherits(x, 'ratetable')) return(F)
+#
+# SCCS $Id: is.ratetable.s,v 4.4 1996-04-09 07:49:47 therneau Exp $
+#
+is.ratetable <- function(x, verbose=F) {
+    msg <- ""
+    if (!inherits(x, 'ratetable')) msg <- c(msg, "wrong class")
     att <- attributes(x)
     if (any(is.na(match(c("dim", "dimnames", "dimid", "factor", "cutpoints"),
-			names(att))))) return(F)
+			names(att))))) msg <- c(msg, 'missing an attribute')
     nd <- length(att$dim)
-    if (length(x) != prod(att$dim)) return(F)
-    if (!(is.list(att$dimnames) && is.list(att$cutpoints))) return(F)
+    if (length(x) != prod(att$dim)) msg <- c(msg, 'dims dont match length')
+    if (!(is.list(att$dimnames) && is.list(att$cutpoints)))
+	     msg <- c(msg, 'dimnames or cutpoints not a list')
     if (length(att$dimnames)!=nd || length(att$factor)!=nd ||
-		     length(att$cutpoints)!=nd) return(F)
+		     length(att$cutpoints)!=nd) msg <- c(msg, 'bad lengths')
     fac <- as.numeric(att$factor)
-    if (any(is.na(fac))) return(F)
-    if (any(fac <0)) return(F)
+    if (any(is.na(fac))) msg <- c(msg, 'a missing factor')
+    if (any(fac <0)) msg <- c(msg, 'factor <0')
     for (i in 1:nd) {
 	n <- att$dim[i]
-	if (length(att$dimnames[[i]]) !=n) return(F)
-	if (fac[i]!=1 && length(att$cutpoints[[i]]) != n)  return(F)
-	if (fac[i]!=1 && any(order(att$cutpoints[[i]])!= 1:n)) return(F)
-	if (fac[i]==1 && !is.null(att$cutpoints[[i]]))  return(F)
-	if (fac[i]>1  && i<nd) return(F)
+	if (length(att$dimnames[[i]]) !=n) msg <- c(msg, 'dimnames wrong length')
+	if (fac[i]!=1 && length(att$cutpoints[[i]])!=n) msg <- c(msg, 'cutpnt missing')
+	if (fac[i]!=1 && any(order(att$cutpoints[[i]])!= 1:n)) msg <- c(msg, 'unsorted cutpoints')
+	if (fac[i]==1 && !is.null(att$cutpoints[[i]]))  msg <- c(msg, 'cutpnt should be null')
+	if (fac[i]>1 && i<nd) msg <- c(msg, 'only the last dim can be interpolated')
 	}
-    T
+    if (msg=='') T
+    else { if (verbose) msg  else F}
     }
