c2dec668a39790fcd5f749b9d20fb282f42c2349
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1998-08-30 16:04:24 +0000

    Remove redundant old code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10228 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/untangle.specials.s
+++ trunk/survival/trunk/untangle.specials.s
@@ -1,25 +1,13 @@
-#SCCS $Id: untangle.specials.s,v 4.2 1997-03-28 09:49:12 therneau Exp $
+#SCCS $Id: untangle.specials.s,v 4.3 1998-08-30 16:04:24 therneau Exp $
 untangle.specials <- function(tt, special, order=1) {
-    #
-    # There was a change in the specials, so action depends on your release
-    #   of S
-    #
+
     spc <- attr(tt, 'specials')[[special]]
     if (length(spc)==0)
 	return(list(vars=character(0), terms=numeric(0)))
 
     facs <- attr(tt, 'factor')
     fname <- dimnames(facs)
-
-    if ((attr(terms(y~ zed(x), specials='zed'), 'specials'))$zed ==1) {
-	# old style
-	if (any(order>1))
-	   warning("Can't select specials based on the order of the terms")
-	list(vars=(fname[[2]])[spc],  terms=spc)
-	}
-    else {
-	ff <- apply(facs[spc,,drop=F], 2, sum)
-	list(vars= (fname[[1]])[spc],
+    ff <- apply(facs[spc,,drop=F], 2, sum)
+    list(vars= (fname[[1]])[spc],
 	     terms= seq(ff)[ff & match(attr(tt, 'order'), order, nomatch=0)])
-	}
     }
