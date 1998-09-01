4beafe822f3c19f80b96beb92cab8a4b692a177b
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1998-09-01 09:46:28 +0000

    Still working on a joint Linux/Sun version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10232 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survS.h
+++ trunk/survival/trunk/survS.h
@@ -1,7 +1,16 @@
-/* SCCS $Id: survS.h,v 5.1 1998-08-30 14:46:35 therneau Exp $
+/* SCCS $Id: survS.h,v 5.2 1998-09-01 09:46:28 therneau Exp $
 /*
-** some macros
+** The next line is needed on Sun: S.h includes time.h, which defines
+**   the variable "time", a variable name that I use often.
+** But on Linux, you must leave the line out: time.h does not define "time",
+**   and "select.h" (also pulled in by S.h) depends on time.h.
+** Real solution: offer the user something other than S.h, which defines
+**   what he needs without including the zillion .h files a compile of S needs!
 */
+
+/* #if (defined(SUNOS))   ok- this didn't work, how do we do it? */
 #define _TIME_H   /* a hack to stop inclusion of time.h */
+/*  #endif  */
+
 #include "S.h"
 #define ALLOC(a,b) S_alloc(a,b,S_evaluator)
