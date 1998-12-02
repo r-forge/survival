d37beed276b0b3b1644b688b078f1ae8bf6225b3
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1998-12-02 13:35:07 +0000

    V5 changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10313 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survreg.d
+++ trunk/survival/trunk/survreg.d
@@ -4,9 +4,8 @@
 Regression for a parametric survival model
 .CS
 survreg(formula, data=sys.parent(), subset, na.action,
-link=c("log", "identity"),
-dist=c("extreme", "logistic", "gaussian", "exponential"),
-fixed, eps=0.0001, init, iter.max=10, model=F, x=F, y=F, ...)
+dist="weibull", init, scale=0, control,
+model=F, x=F, y=T, ...)
 .AG formula
 a formula expression as for other regression models.
 See the documentation for `lm' and `formula' for details.
@@ -18,20 +17,24 @@ formula.
 subset of the observations to be used in the fit.
 .AG na.action
 function to be used to handle any NAs in the data.
-.AG link
-transformation to be used on the y variable.
 .AG dist
-assumed distribution for the transformed y variable.
-.AG fixed
-a list of fixed parameters, most often just the scale.
-(When I implement the t-dist, it will include the degrees of freedom).
-.AG eps
-convergence criteria for the computation.  Iteration continues until the
-relative change in log likelihood is less than eps.
+assumed distribution for y variable.  These include
+weibull, exponential, gaussian, logistic, lognormal and loglogistic.
+If the argument is a character string, then it is assumed to name an
+element from `survreg.distributions' (enter `names(survreg.distributions)'
+for a full list of choices).
+Otherwise, it is assumed to be a user defined list conforming to this
+standard.
+.AG parm
+a list of fixed parameters.  For the t-distribution for instance this is
+the degrees of freedom; most of the distributions have no parameters.
 .AG init
 optional vector of initial values for the paramters.
-.AG iter.max
-maximum number of iterations to be performed.
+.AG scale
+optional fixed value for the scale.  If set to <=0 then the scale is
+estimated.
+.AG control
+a list of control values, in the format producted by `survreg.control'.
 .AG model
 if TRUE, the model frame is returned.
 .AG x
@@ -39,16 +42,17 @@ if TRUE, then the X matrix is returned.
 .AG y
 if TRUE, then the y vector (or survival times) is returned.
 .AG ...
-all the optional arguments to lm, including `singular.ok'.
+other arguments which will be passed to `survreg.control'.
 .RT
-an object of class `survreg' is returned, which inherits from class `glm'.
+an object of class `survreg' is returned.
+.SH Note
+This routine underwent significant changes from survival4 to survival5.
 .SH Computation
-  This routine is not as robust against nearly singular X matrices as lm();
-the problem occurs when we explicitly invert the covariance matrix with
-solve().  This can sometimes be solved by subtracting the mean from all
-continuous covariates.
+The routine uses a Newton-Raphson iteration with step halving, 
+with provision for general penalized term.
+Fisher scoring is used for itermediate steps where the information matrix
+is not positive definite.
 .EX
-survreg(Surv(futime, fustat) ~ ecog.ps + rx, fleming, dist='extreme',
-		link='log', fixed=list(scale=1))   #Fit an exponential
+survreg(Surv(futime, fustat) ~ ecog.ps + rx, fleming, dist='lognormal')
 .KW survival
 .WR
