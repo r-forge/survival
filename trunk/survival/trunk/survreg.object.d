1aac41ce446f62259de02d799aac82d82c94b4e7
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1998-12-04 09:32:02 +0000

    Update to v5
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10318 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survreg.object.d
+++ trunk/survival/trunk/survreg.object.d
@@ -5,17 +5,10 @@ Parametric Survival Model Object
 .PP
 This class of objects is returned by the `survreg' function
 to represent a fitted parametric survival model.
-Class `survreg' inherits from class `glm', since it is fit by iterative
-reweighted least squares; the object returned has all the components of a
-weighted least squares object.
-
 Objects of this class have methods for the functions `print',
 `summary', `predict', and 'residuals'.
 .SH COMPONENTS
 The following components must be included in a legitimate `survreg' object.
-The residuals, fitted values, coefficients and effects should be extracted
-by the generic functions of the same name, rather than
-by the `"$"' operator. 
 .AG coefficients
 the coefficients of the `linear.predictors', which multiply  the
 columns of the model
@@ -27,36 +20,38 @@ model matrix).
 If the model is over-determined there will
 be missing values in the coefficients corresponding to inestimable
 coefficients.
-.AG parms
-the parameters of the model that are not coefficients of the X matrix.
-The first of these will always be `log(sigma)'.
-.AG fixed
-a vector of the same length as `parms', where 1 indicates a parameter that
-was fixed at its starting value and was not part of the iteration.
-.AG deviance
-minus twice the difference between the maximized log-likelihood under the
-fitted model and a saturated model.
-Similar to the residual sum of squares.
-.AG loglik
-the log-likelihood for the final model.
-.AG null.deviance
-the deviance corresponding to the model with only an itercept term, and
-with `parms' fixed at their final values.
-.AG dresiduals
-the deviance residuals.
+.AG icoef
+coefficients of the baseline model, which will contain the intercept
+and log(scale), or mulitple scale factors for a stratified model.
 .AG var
-the final variance matrix, including both coefficients and free parameters.
-.AG family
-a 2 element character vector giving the name of the family and
-the link; mainly for printing purposes.
+the variance-covariance matrix for the parameters, including the log(scale)
+parameter(s).
+.AG loglik
+a vector of length 2, containing the log-likelihood for the baseline and
+full models. 
+.AG iter 
+the number of iterations required
+.AG linear.predictors
+the linear predictor for each subject.
+.AG df
+the degrees of freedom for the final model.  For a penalized model
+this will be a vector with one element per term.
+.AG scale
+the scale factor(s), with length equal to the number of strata.
+.AG idf
+degrees of freedom for the initial model.
+.AG means
+a vector of the column means of the coefficient matrix.
+.AG dist
+the distribution used in the fit.
 .PP
-The object will also have the components of an `glm' object:
-`linear predictors', `fitted.values', `residuals',
-`effects', `R', `rank', `assign', `contrasts', `weights', `iter',
-`residuals', `fitted.values', `call', `terms' and `formula'.
-See `glm.object'.
+The object will also have the following components found in 
+other model results (some are optional):
+`linear predictors', `weights', 'x', 'y', 'model', 
+`call', `terms' and `formula'.
+See `lm.object'.
 .SA
-`survreg', `glm.object', `lm.object'.
+`survreg'
 .KW regression
 .KW survival
 .WR
