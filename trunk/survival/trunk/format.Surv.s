0f199ca8b3de79429475fdda46530f5a1d45b391
therneau (Terry Therneau) <therneau.terry@mayo.edu>
1998-12-22 08:54:21 +0000

    Add the version specific choice of as.data.frame vs data.frameAux
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10327 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/format.Surv.s
+++ trunk/survival/trunk/format.Surv.s
@@ -1,7 +1,14 @@
-# SCCS $Id: format.Surv.s,v 4.5 1998-08-30 15:18:37 therneau Exp $
-#
-# These two functions operate with the newer data.frame code, found on statlib
-#   (Eventually part of S, I assume)
+# SCCS $Id: format.Surv.s,v 4.6 1998-12-22 08:54:21 therneau Exp $
 #
 format.Surv <- function(x, ...) format(as.character.Surv(x), ...)
-as.data.frame.Surv <- as.data.frame.model.matrix
+
+# The function to "make something suitable for inclusion in a data frame"
+#   was "as.data.frame.x" in versions <5, now it is "data.frameAux.x",
+#   so here we have a version specific definition.
+
+if (version$major >= 5) {
+    data.frameAux.Surv <- function(x, ...) data.frameAux.AsIs(x, ...)
+    }
+else {
+    as.data.frame.Surv <- as.data.frame.model.matrix
+    }
