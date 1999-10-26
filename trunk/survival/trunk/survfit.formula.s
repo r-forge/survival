fc5590dafb4352598d5865f9271b2e448c6c13e5
boos <unknown@noreply.com>
1999-10-26 14:45:38 +0000

    Initial revision
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10402 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/survival/trunk/survfit.formula.s
@@ -0,0 +1,48 @@
+#SCCS $Date: 1999-10-26 14:43:35 $ $Id: survfit.formula.s,v 1.1 1999-10-26 14:43:35 boos Exp $
+survfit.formula <- function(formula, data, weights, subset, 
+			    na.action, call, ...) {
+    if (missing(call)) call <- match.call()
+    m <- match.call(expand=T)
+    m <- m[match(c("", "data", "weights", "subset", "na.action"), 
+		 names(m), nomatch=0)]
+    
+    Terms <- terms(formula, 'strata')
+    ord <- attr(Terms, 'order')
+    if (length(ord) & any(ord !=1))
+	    stop("Interaction terms are not valid for this function")
+
+    m$formula <- Terms
+    m[[1]] <- as.name("model.frame")
+    m <- eval(m, sys.parent())
+
+    n <- nrow(m)
+    Y <- model.extract(m, response)
+    if (!is.Surv(Y)) stop("Response must be a survival object")
+
+    casewt <- model.extract(m, "weights")
+
+    if (is.null(casewt)) casewt <- rep(1,n)
+
+    if (!is.null(attr(Terms, 'offset'))) warning("Offset term ignored")
+
+    ll <- attr(Terms, 'term.labels')
+    if (length(ll) == 0) X <- factor(rep(1,n))  # ~1 on the right
+    else X <- strata(m[ll])
+    
+    temp <- survfit.km(X, Y, casewt, ...)
+    oldClass(temp) <- "survfit"
+    if (!is.null(attr(m, 'na.action')))
+	    temp$na.action <- attr(m, 'na.action')
+
+    temp$call <- call
+    temp
+    }
+
+
+
+
+
+
+
+
+
