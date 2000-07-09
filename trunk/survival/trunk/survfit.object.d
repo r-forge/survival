757359d8bf346c370cd9a3f493221a7cdb456da2
boos <unknown@noreply.com>
2000-07-09 14:40:16 +0000

    Updated help to match Splus help and added info for stop/start times and variables to handle counts - boos
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10464 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/survfit.object.d
+++ trunk/survival/trunk/survfit.object.d
@@ -3,26 +3,44 @@
 .TL
 Survival Curve Object
 .DN
-This class of objects is returned by the `survfit' class of functions
-to represent a fitted survival curve.
-
-Objects of this class have methods for the functions `print',
-`summary', `plot', `points' and `lines'.
-.SH COMPONENTS
-The following components must be included in a legitimate `survfit' object.
+Class of objects that represent a fitted survival curve.
+.SH GENERATION
+This class of objects is returned by the 'survfit' class of functions.
+.SH METHODS
+Objects of this class have methods for the functions 'print',
+'summary', 'plot', 'points' and 'lines'.
+.SH STRUCTURE
+The following components must be included in a legitimate `survfit'
+object.
+.AG n
+total number of subjects.
 .AG time
 the time points at which the curve has a step.
 .AG n.risk
 the number of subjects at risk at t.
 .AG n.event
 the number of events that occur at time t.
+.AG enter
+if 'type' argument is 'counting', the number of subjects that enter at time t.
+.AG exit.censor
+if 'type' argument is 'counting', the number of subjects that leave
+without an event  at time t.
 .AG surv
 the estimate of survival at time t+0.
 This may be a vector or a matrix.
+.AG strata.all
+if there are multiple curves, this component gives the total number of elements of
+the `time' etc. vectors before any subsetting, corresponding to the first curve, the second curve,
+and so on.  The names of the elements are labels for the curves.
 .AG strata
 if there are multiple curves, this component gives the number of elements of
 the `time' etc. vectors corresponding to the first curve, the second curve,
 and so on.  The names of the elements are labels for the curves.
+.AG ntimes.strata
+if there are multiple curves, this component gives the number of
+elements of
+the `time' etc. vectors for the first curve, the second curve,
+and so on.
 .AG std.err
 the standard error of the cumulative hazard or -log(survival).
 .AG upper
@@ -38,7 +56,9 @@ the returned value from the na.action function, if any.  It will be used
 in the printout of the curve, e.g., the number of observations deleted due
 to missing values.
 .AG call
-the call that produced the object.
+an image of the call that produced the object.
+.AG type
+type of survival censoring.
 .SH SUBSCRIPTS
 Survfit objects that contain multiple survival curves can be subscripted.
 This is most often used to plot a subset of the curves.
@@ -48,6 +68,10 @@ a `strata' statement \(em there is a matrix of curves and 2 subscripts may
 be used.
 (In this case `summary.survfit' will also print the data as a matrix).
 .SA
-`survfit', `plot.survfit', `summary.survfit'.
-.KW survival
+`plot.survfit', `summary.survfit', `survfit'.
+.KW survival4
 .WR
+
+
+
+
