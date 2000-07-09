3068287d6dd32aa084caba421328e1571dd1b34c
boos <unknown@noreply.com>
2000-07-09 14:17:11 +0000

    Updated help to match Splus help - boos
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10452 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/Surv.d
+++ trunk/survival/trunk/Surv.d
@@ -8,7 +8,6 @@ Create a survival object, usually used as a response variable in a model
 formula.
 .CS
 Surv(time, event)
-or
 Surv(time, time2, event, type=<<see below>>, origin=0)
 is.Surv(x)
 .RA
@@ -18,51 +17,50 @@ first argument is the starting time for the interval.
 .AG x
 any S-PLUS object.
 .OA
-.AG event
-The status indicator, normally 0=alive, 1=dead.  Other choices are `T/F'
-(TRUE = death) or 1/2 (2=death).
-For interval censored data, the status indicator is 0=right censored,
-1= event at `time', 2=left censored, 3=interval censored.
-Although unusual, the event indicator can be omitted,
-in which case all subjects are assumed to have an event.
 .AG time2
 ending time of the interval for interval censored  or counting process 
 data only. 
 Intervals are
-assumed to be open on the left and closed on the right, `(start, end]'.
+assumed to be open on the left and closed on the right, (start, end].
 For counting process data,
-`event' indicates whether an event occurred at the end of the interval.
+'event' indicates whether an event occurred at the end of the interval.
+.AG event
+status indicator, normally 0 = alive and 1 = dead.  Other choices are T/F
+(T = death) or 1/2 (2 = death).
+For interval censored data, the status indicator is 0 = right censored,
+1 = event at `time', 2 = left censored, and 3 = interval censored.
+For left-censored data, the status indicator is T=uncensored, F=censored.
+Although unusual, the event indicator can be omitted,
+in which case all subjects are assumed to have an event.
 .AG type
 character string specifying the type of censoring. Possible values
 are `"right"', `"left"', `"counting"', `"interval"', or `"interval2"'.
 The default is `"right"' or `"counting"' depending on whether the `time2'
 argument is absent or present, respectively.
 .AG origin
-for counting process data, the hazard function origin.  This is most often
-used in conjunction with a model containing time dependent strata in order
-to align the subjects properly when they cross over from one strata to
+hazard function origin for counting process data.  Most often
+used in a model containing time dependent strata 
+to align the subjects properly when they change from one strata to
 another.
 .RT
-An object of class `Surv'.  There are methods for `print', `is.na', and
-subscripting survival objects.  To include a survival object inside a
-data frame, use the `I()' function.  `Surv' objects are implemented as
-a matrix of 2 or 3 columns.
-.PP
-In the case of `is.Surv', a logical value `T' if `x' inherits from 
-class `"Surv"', otherwise an `F'.
+in the case of 'Surv', a matrix of 2 or 3 columns of class '"Surv"'
+containing 'time', 'time2' (if provided), and 'status'.
+.PP 
+In the case of 'is.Surv', a logical value 'T' if 'x' inherits from class
+'"Surv"', otherwise an 'F'.
 .SH DETAILS
 In theory it is possible to represent interval censored data without a
 third column containing the explicit status.  Exact, right censored,
 left censored and interval censored observation would be represented as
-intervals of `[a,a]', `[a, infinity)', `(-infinity,b]', and `[a,b)' 
-respectively; each specifying the interval within which the event is 
+intervals of [a,a], [a, infinity), (-infinity,b], and [a,b) 
+respectively; each interval is a pair of time points within which the event is 
 known to have occurred.
 .PP
-If `type = "interval2"' then the representation given above is
-assumed, with `NA' taking the place of infinity.  If `type="interval"'
+If `type="interval2"', the representation given above is
+assumed, with `NA' taking the place of infinity.  If `type="interval"',
 `event' must be given.  If `event' is `0', `1', or `2', the relevant
 information is assumed to be contained in `time', the value in `time2'
-is ignored, and the second column of the result will contain a
+is ignored, and the second column of the result contains a
 placeholder.
 .PP
 Presently, the only methods allowing interval censored data are the
@@ -80,8 +78,8 @@ guess wrong.  Use 0/1 coding in this case.
 `coxph', `survfit', `survreg'.
 .EX
 Surv(leukemia$time, leukemia$status)
- [1] 9    13   13+  18   23   28+  31   34   45+  48   161+ 5    5    8    8
- [16] 12   16+  23   27   30   33   43   45
 Surv(heart$start, heart$stop, heart$event)
-.KW survival
+.KW survival4
 .WR
+
+
--- trunk/survival/trunk/lines.survfit.d
+++ trunk/survival/trunk/lines.survfit.d
@@ -3,7 +3,7 @@
 .TL
 Add Lines to a Survival Plot
 .DN
-Often used to add the expected survival curve(s) to a Kaplan-Meier plot
+Usually used to add the expected survival curve(s) to a Kaplan-Meier plot
 generated with `plot.survfit'.
 .CS
 lines.survfit(x, type="s", mark=3, col=1, lty=1, lwd=1, mark.time=T,
@@ -26,10 +26,10 @@ If `mark.time' is a numeric vector, then curves are marked at
 the specified time points.
 .AG xscale
 a number used to divide the x values.  If time was originally in days, a
-value of 365.24 would give a plotted scale in years.
+value of 365.25 would give a plotted scale in years.
 .AG firstx, firsty
 the starting point for the survival curves.  If either of these is set to
-`NA' or <blank> the plot will start at the first time point of the curve.
+`NA' or < blank > the plot will start at the first time point of the curve.
 .AG xmax
 the maximum horizontal plot coordinate.    
 This shortens the curve before plotting it, so unlike using the
@@ -40,18 +40,18 @@ an arbitrary function defining a transformation of the survival curve.
 For example `fun=log' is an alternative way to draw a log-survival curve
 (but with the axis labeled with log(S) values).
 Four often used transformations can be specified with a character
-argument instead: `"log"' is the same as using the `log=T' option,
-`"event"' plots cumulative events (f(y) =1-y),
-`"cumhaz"' plots the cumulative hazard function (f(y) = -log(y))
-and `"cloglog"' creates a complementary log-log survival plot 
+argument instead: "log" is the same as using the `log=T' option,
+"event" plots cumulative events (f(y) = 1-y),
+"cumhaz" plots the cumulative hazard function (f(y) = -log(y))
+and "cloglog" creates a complimentary log-log survival plot 
 (f(y) = log(-log(y) along with log scale for the x-axis).
 .AG conf.int
-if TRUE, confidence bands for the curves are also plotted.
-If set to `"only"', then only the CI bands are plotted, and the curve
+if 'TRUE', confidence bands for the curves are also plotted.
+If set to "only", then only the CI bands are plotted, and the curve
 itself is left off.  
 This can be useful for fine control over the colors or line types of a plot.
 .RT
-a list with components `x' and `y', containing the coordinates of the
+a list with components x and y, containing the coordinates of the
 last point on each of the curves (but not of the confidence limits).
 This may be useful for labeling.
 .SE
@@ -59,20 +59,20 @@ one or more curves are added to the current plot.
 .SA
 `lines', `par', `plot.survfit', `survfit', `survexp'.
 .EX
-fit <- survfit(Surv(futime, status==2) ~ sex, pbc)
-plot(fit, mark.time=F, xscale=365.24,
-	xlab='Years', ylab='Survival')
-lines(fit[1], lwd=2, xscale=365.24)    #darken the first curve and add marks
-
-# add expected survival curves for the two groups,
-#   based on the US expected
-tdata <- data.frame(age=pbc$age*365.24, sex=pbc$sex +1, 
-		    year= rep(mdy.date(1,1,1976), nrow(pbc)))
-efit <- survexp(~ sex, data=tdata, ratetable=survexp.us, 
-		    times=(0:24)*182)
-temp <- lines(efit, lty=2, xscale=365.24, lwd=2:1)
-text(temp, c("Male", "Female"), adj= -.1) #labels just past the ends
-
-title(main="Primary Biliary Cirrhosis, Observed and Expected")
-.KW survival
+# First select by "id" those elements with the greater value of "stop"
+hearta <- by(heart, heart$id, function (x)x[x$stop==max(x$stop),])
+# Make the resulting list into a data frame by rebinding its
+components
+hearta <- do.call("rbind", hearta)
+# plot the fitted survival curves
+plot(survfit(Surv(stop, event) ~ surgery, data = hearta), lty = 1:2)
+# Add lines denoting the expected survival from National Rate Table
+lines(survexp (stop ~ surgery + ratetable(age = (age + 48) * 365.25,
+      sex = "male", year = year * 365.25), data = hearta), mark = "E", 
+      mark.time = 1000, lty = 1:2, cex = 1.5)
+.KW survival4
 .WR
+
+
+
+
