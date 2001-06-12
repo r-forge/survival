bf0bb6225f22a001ca34b085873315f29343453d
therneau (Terry Therneau) <therneau.terry@mayo.edu>
2001-06-12 08:53:58 +0000

    Fix bug in 'origin'.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10491 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/Surv.s
+++ trunk/survival/trunk/Surv.s
@@ -1,52 +1,55 @@
-#SCCS $Id: Surv.s,v 5.5 2000-07-09 14:15:40 boos Exp $
+#SCCS $Id: Surv.s,v 5.6 2001-06-12 08:53:57 therneau Exp $
 # Package up surivival type data as a structure
 #
 Surv <- function(time, time2, event,
 	      type=c('right', 'left', 'interval', 'counting', 'interval2'),
 		       origin=0) {
+
+    if (missing(time)) stop ("Must have a time argument")
+    if (!is.numeric(time)) stop ("Time variable is not numeric")
     nn <- length(time)
-    ng <- nargs()
+    if (length(origin)!=1 && length(origin)!=nn) 
+	    stop("Wrong length for origin")
+
+    ng <- 1 + (!missing(time2)) + (!missing(event))  #of first 3 present
+    # The logic below uses "ng" throughout; why not use "missing(time2)"
+    # and missing(status) instead?  Because we want to assume that 
+    # "Surv(a,b)" has the variable b matched to status rather than time2.
+    #
+
     if (missing(type)) {
 	if (ng==1 || ng==2) type <- 'right'
 	else if (ng==3)     type <- 'counting'
-	else stop("Invalid number of arguments")
 	}
     else {
 	type <- match.arg(type)
-	ng <- ng-1
 	if (ng!=3 && (type=='interval' || type =='counting'))
 		stop("Wrong number of args for this type of survival data")
 	if (ng!=2 && (type=='right' || type=='left' ||  type=='interval2'))
 		stop("Wrong number of args for this type of survival data")
 	}
-    who <- !is.na(time)
 
     if (ng==1) {
-	if (!is.numeric(time)) stop ("Time variable is not numeric")
-	ss <- cbind(time, 1)
-	dimnames(ss) <- list(NULL, c("time", "status"))
+	ss <- cbind(time=time-origin, status=1)
 	}
     else if (type=='right' || type=='left') {
-	if (!is.numeric(time)) stop ("Time variable is not numeric")
 	if (length(time2) != nn) stop ("Time and status are different lengths")
 	if (is.logical(time2)) status <- 1*time2
-	    else  if (is.numeric(time2)) {
-		who2 <- !is.na(time2)
-		if (max(time2[who2]) ==2) status <- time2 -1
-		else status <- time2
-		if (any(status[who2] !=0  & status[who2]!=1))
+	else  if (is.numeric(time2)) {
+	    who2 <- !is.na(time2)
+	    if (max(time2[who2]) ==2) status <- time2 -1
+	    else status <- time2
+	    if (any(status[who2] !=0  & status[who2]!=1))
 				stop ("Invalid status value")
-		}
-	    else stop("Invalid status value")
-	 ss <- cbind(time, status)
-	 dimnames(ss) <- list(NULL, c("time", "status"))
+	    }
+	else stop("Invalid status value")
+	ss <- cbind(time=time-origin, status=status)
 	}
     else  if (type=='counting') {
 	if (length(time2) !=nn) stop ("Start and stop are different lengths")
 	if (length(event)!=nn) stop ("Start and event are different lengths")
-	if (!is.numeric(time))stop("Start time is not numeric")
 	if (!is.numeric(time2)) stop("Stop time is not numeric")
-	who3 <- who & !is.na(time2)
+	who3 <- !(is.na(time) | is.na(time2))
 	if (any (time[who3]>= time2[who3]))stop("Stop time must be > start time")
 	if (is.logical(event)) status <- 1*event
 	    else  if (is.numeric(event)) {
@@ -57,11 +60,12 @@ Surv <- function(time, time2, event,
 				stop("Invalid status value")
 		}
 	    else stop("Invalid status value")
-	ss <- cbind(time-origin, time2-origin, status)
+	ss <- cbind(start=time-origin, stop=time2-origin, status)
 	}
 
     else {  #interval censored data
 	if (type=='interval2') {
+	    if (!is.numeric(time2)) stop("Time2 must be numeric")
 	    event <- ifelse(is.na(time), 2,
 		     ifelse(is.na(time2),0,
 		     ifelse(time==time2, 1,3)))
@@ -77,8 +81,9 @@ Surv <- function(time, time2, event,
 		stop("Status indicator must be 0, 1, 2 or 3")
 	    }
 	status <- event
-	ss <- cbind(time, ifelse(!is.na(event) & event==3, time2, 1),
-			    status)
+	ss <- cbind(time1=time-origin, 
+		    time2=ifelse(!is.na(event) & event==3, time2-origin, 0),
+		    status=status)
 	}
 
     attr(ss, "type")  <- type
@@ -86,8 +91,10 @@ Surv <- function(time, time2, event,
     ss
     }
 
-print.Surv <- function(xx, quote=F, ...)
-    invisible(print(as.character.Surv(xx), quote=quote, ...))
+print.Surv <- function(x, quote=F, ...) {
+    print(as.character.Surv(x), quote=quote, ...)
+    invisible(x)
+    }
 
 as.character.Surv <- function(xx) {
     oldClass(xx) <- NULL
