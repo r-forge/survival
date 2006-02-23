2e4db5f07eb5b4636d25cff4f1739ae4dc76e25c
lunde <unknown@noreply.com>
2006-02-23 23:01:48 +0000

    Made some bug-fix changes on behalf of Beth Atkinson.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10576 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/summary.ratetable.s
+++ trunk/survival/trunk/summary.ratetable.s
@@ -1,32 +1,25 @@
-# SCCS $Id: summary.ratetable.s,v 1.1 1998-11-03 20:26:19 therneau Exp $
+# SCCS $Id: summary.ratetable.s,v 1.2 2006-02-23 23:01:48 lunde Exp $
 #
 # Print out information about a rate table: it's dimensions and keywords
 #
-summary.ratetable <- function(rtable, ...) {
-    if (!inherits(rtable, 'ratetable')) stop("Argument is not a rate table")
-
-    att <- attributes(rtable)
-    ncat <- length(dim(rtable))
-    cat (" Rate table with", ncat, "dimensions:\n")
-    for (i in 1:ncat) {
-	if (att$factor[i]==0) {
-	    cat("\t", att$dimid[i], " ranges from ", 
-		format(min(att$cutpoints[[i]])), " to ", 
-		format(max(att$cutpoints[[i]])), "; with ", att$dim[i],
-		" categories\n", sep='')
-	    }
-	else if(att$factor[i]==1) {
-	     cat("\t", att$dimid[i], " has levels of: ",
-		 paste(att$dimnames[[i]], collapse=' '), "\n", sep='')
-	     }
-	else {
-	    cat("\t", att$dimid[i], " ranges from " , 
-		format(min(att$cutpoints[[i]])), " to ", 
-		format(max(att$cutpoints[[i]])), "; with ", att$dim[i],
-		" categories,\n\t\tlinearly interpolated in ",
-		att$factor[i], " steps per division\n", sep='')
-	    }
-	}
-    invisible(att)
+summary.ratetable <- function(x, ...) {  
+  alist <- attributes(x)
+  ndim <- length(alist$dim)
+  if(ndim > 1)
+    cat("Rate table with dimensions:\n")
+  else cat("Rate table with 1 dimension:\n")
+  fac <- alist$factor
+  temp1 <- format(alist$dimid)
+  for(i in 1:ndim) {
+    cat("    ", temp1[i], ": ", sep = "")
+    if(fac[i] == 1) {
+      cat("discrete factor with legal values of (",
+          paste(alist$dimnames[[i]], collapse = ", "), ")\n", sep = "")
     }
-
+    else if(fac[i] == 0)
+      cat("time variable with", format(alist$dim[i]), 
+          "categories\n")
+    else cat("time variable with", format(alist$dim[i]), 
+             "categories (interpolated)\n")
+  }
+}
