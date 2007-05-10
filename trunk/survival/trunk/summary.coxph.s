a7a5675e6c0aef808313c9d3319579a84e26d56b
therneau (Terry Therneau) <therneau.terry@mayo.edu>
2007-05-10 18:45:52 +0000

    Change the summary.coxph method so that it returns an object, and add a print method for this 'summary.coxph' object.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@10635 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/survival/trunk/summary.coxph.s
+++ trunk/survival/trunk/summary.coxph.s
@@ -1,90 +1,51 @@
-# $Date: 2006-08-28 15:39:49 $ $Id: summary.coxph.s,v 4.9 2006-08-28 15:39:49 m015733 Exp $
+# $Date: 2007-05-10 18:45:52 $ $Id: summary.coxph.s,v 4.10 2007-05-10 18:45:52 therneau Exp $
 summary.coxph <-
- function(cox, table = TRUE, coef = TRUE, conf.int = 0.95, scale = 1,
-			digits = max(options()$digits - 4, 3))
-    {
-    if (!is.null(cl<- cox$call)) {
-	cat("Call:\n")
-	dput(cl)
-	cat("\n")
-	}
-    if (!is.null(cox$fail)) {
-	cat(" Coxreg failed.", cox$fail, "\n")
-	return()
-	}
-    savedig <- options(digits = digits)
-    on.exit(options(savedig))
-
-    omit <- cox$na.action
-    if (length(omit))
-	cat("  n=", cox$n, " (", naprint(omit), ")\n", sep="")
-    else cat("  n=", cox$n, "\n")
-    if (length(cox$icc))
-	cat("  robust variance based on", cox$icc[1],
-	    "groups, intra-class correlation =", format(cox$icc[2:3]), "\n")
+ function(object, conf.int = 0.95, scale = 1,
+          digits = max(options()$digits - 4, 3)) {
+    cox <- object
     if (is.null(cox$coef)) {   # Null model
-	cat ("   Null model\n")
-	return()
+	return(object)  #The summary method is the same as print in this case
 	}
 
+    #
+    # Create the matrix of coefficients
     beta <- cox$coef
     nabeta <- !(is.na(beta))          #non-missing coefs
     beta2 <- beta[nabeta]
     if(is.null(beta) | is.null(cox$var))
-        stop("Input is not valid")
+        stop("Input is not a valid coxph fit")
     se <- sqrt(diag(cox$var))
     if (!is.null(cox$naive.var)) nse <- sqrt(diag(cox$naive.var))
 
-    if(coef) {
-	if (is.null(cox$naive.var)) {
-	    tmp <- cbind(beta, exp(beta), se, beta/se,
+    if (is.null(cox$naive.var)) {
+        coef <- cbind(beta, exp(beta), se, beta/se,
 		   signif(1 - pchisq((beta/ se)^2, 1), digits -1))
-	    dimnames(tmp) <- list(names(beta), c("coef", "exp(coef)",
+        dimnames(coef) <- list(names(beta), c("coef", "exp(coef)",
 		"se(coef)", "z", "p"))
-	    }
-	else {
-	    tmp <- cbind(beta, exp(beta), nse, se, beta/se,
-		   signif(1 - pchisq((beta/ se)^2, 1), digits -1))
-	    dimnames(tmp) <- list(names(beta), c("coef", "exp(coef)",
-		"se(coef)", "robust se", "z", "p"))
-	    }
-        cat("\n")
-        prmatrix(tmp)
         }
-    if(conf.int) {
-        z <- qnorm((1 + conf.int)/2, 0, 1)
-        beta <- beta * scale
-        se <- se * scale
-        tmp <- cbind(exp(beta), exp(.Uminus(beta)), exp(beta - z * se),
+    else {
+        coef <- cbind(beta, exp(beta), nse, se, beta/se,
+		   signif(1 - pchisq((beta/ se)^2, 1), digits -1))
+        dimnames(coef) <- list(names(beta), c("coef", "exp(coef)",
+		"se(coef)", "robust se", "z", "p"))
+        }
+
+    # Create the matrix of confidence intervals
+    z <- qnorm((1 + conf.int)/2, 0, 1)
+    beta <- beta * scale
+    se <- se * scale
+    cimat <- cbind(exp(beta), exp(.Uminus(beta)), exp(beta - z * se),
             exp(beta + z * se))
-        dimnames(tmp) <- list(names(beta), c("exp(coef)", "exp(-coef)",
+    dimnames(cimat) <- list(names(beta), c("exp(coef)", "exp(-coef)",
             paste("lower .", round(100 * conf.int, 2), sep = ""),
             paste("upper .", round(100 * conf.int, 2), sep = "")))
-        cat("\n")
-        prmatrix(tmp)
-        }
-    logtest <- -2 * (cox$loglik[1] - cox$loglik[2])
-    sctest <- cox$score
-    df <- length(beta2)
-    cat("\n")
-    cat("Rsquare=", format(round(1-exp(-logtest/cox$n),3)),
-	"  (max possible=", format(round(1-exp(2*cox$loglik[1]/cox$n),3)),
-	")\n" )
-    cat("Likelihood ratio test= ", format(round(logtest, 2)), "  on ",
-	df, " df,", "   p=", format(1 - pchisq(logtest, df)),
-	"\n", sep = "")
-    cat("Wald test            = ", format(round(cox$wald.test, 2)), "  on ",
-	df, " df,", "   p=", format(1 - pchisq(cox$wald.test, df)),
-	"\n", sep = "")
-    cat("Score (logrank) test = ", format(round(sctest, 2)), "  on ", df,
-        " df,", "   p=", format(1 - pchisq(sctest, df)), sep ="") 
-    if (is.null(cox$rscore)) cat("\n\n")
-    else cat(",   Robust = ", format(round(cox$rscore, 2)), 
-	   "  p=", format(1 - pchisq(cox$rscore, df)), "\n\n", sep="")   
 
-    if (!is.null(cox$naive.var))
-	cat("  (Note: the likelihood ratio and score tests",
-	  "assume independence of\n     observations within a cluster,",
-	    "the Wald and robust score tests do not).\n")
-    invisible()
+
+    result <- list(coefficient=coef, cimat=cimat)
+    result <- c(result, 
+                cox[match(c('na.action', 'call', 'fail', 'icc', 'loglik',
+                          'score', 'rscore', 'naive.var', 'n'), names(cox),
+                        nomatch=0)])
+    oldClass(result) <- 'summary.coxph'
+    result
     }
