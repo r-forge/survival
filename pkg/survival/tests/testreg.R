0d817161584f9c1d59a3b1945c31b0114c60653f
therneau (Terry Therneau) <therneau.terry@mayo.edu>
2009-06-10 22:32:00 +0000

    Error for survreg + user defined dist + interval censored combo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@11285 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/survival/tests/testreg.R
+++ pkg/survival/tests/testreg.R
@@ -1,13 +1,9 @@
-options(na.action=na.exclude) # preserve missings
-options(contrasts=c('contr.treatment', 'contr.poly')) #ensure constrast type
+options(na.action=na.exclude)  #preserve length of missings
 library(survival)
 
 #
-# Run a test that can be verified using other packages (we used SAS)
+# Run a test that can be verified using SAS's LIFEREG
 #
-test1 <- data.frame(time=  c(9, 3,1,1,6,6,8),
-                    status=c(1,NA,1,0,1,1,0),
-                    x=     c(0, 2,1,1,1,0,0))
 fit1w <- survreg(Surv(time, status) ~x, test1, dist='weibull')
 fit1w
 summary(fit1w)
@@ -33,17 +29,28 @@ fitfl <- survreg(Surv(futime, fustat) ~ age + ecog.ps, ovarian,
 	dist='loglogistic')
 fitfl
 
-flem2 <- scan("data.fleming2", what=list(ltime=0, rtime=0))
-flsurv<- Surv(flem2$ltime, flem2$rtime, type='interval2')
+#test out interval censoring, using some dummy time values
 
-fitfw2 <- survreg(flsurv ~ age + ecog.ps, ovarian, dist='weibull')
+idat <- read.table('data.interval', skip=3, header=T, sep=',')
+flsurv<- Surv(idat$ltime, idat$rtime, type='interval2')
+
+fitfw2 <- survreg(flsurv ~ age + ecog.ps, idat, dist='weibull')
 summary(fitfw2)
 
-fitfl2 <- survreg(flsurv ~ age + ecog.ps, ovarian, dist='loglogistic')
+fitfl2 <- survreg(flsurv ~ age + ecog.ps, idat, dist='loglogistic')
 summary(fitfl2)
 
-fitfg2 <- survreg(flsurv ~ age + ecog.ps, ovarian, dist='lognormal')
+fitfg2 <- survreg(flsurv ~ age + ecog.ps, idat, dist='lognormal')
 summary(fitfg2)
+
+logt <- c(survreg.distributions$t, 
+          survreg.distributions$weibull[c('trans', 'itrans', 'dtrans')])
+logt$name <- 'log(t)'
+
+fitft2 <- survreg(Surv(ltime, rtime, type='interval2') ~ age + ecog.ps,
+            idat, dist=logt, parm=100)
+summary(fitft2)  #should be quite close to fitfg2
+
 #
 # Check out the survreg density and probability functions
 #
