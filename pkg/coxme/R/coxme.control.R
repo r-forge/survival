cfaeb18f3ba7c52ea0fcceb1663e8fe1277f189a
therneau (Terry Therneau) <therneau.terry@mayo.edu>
2009-08-07 21:17:07 +0000

    continued updates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@11304 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/coxme/R/coxme.control.R
+++ pkg/coxme/R/coxme.control.R
@@ -7,21 +7,21 @@ coxme.control <- function(eps=1e-8,
                           iter.max =20,
 			  inner.iter=5,
 			  sparse.calc=NULL,
-                          optpar=list(method='BFGS'),
-                          toler.opt=1e-3 )  {
+                          optpar=list(method='BFGS', 
+                                      control=list(reltol=1e-3))) {
     if (iter.max <0) stop("Invalid value for iterations")
     if (inner.iter<1) stop("Invalid value for inner iterations")
     if (eps <=0) stop ("Invalid convergence criteria")
     if (eps <= toler.chol) 
 	    warning("For numerical accuracy, tolerance should be < eps")
-    if (toler.opt <=eps)
-        warning("For numerical accuracy, eps should be < toler.opt")
+    if (optpar$control$reltol <= eps)
+        warning(paste("For numerical accuracy, eps (tolerance for an inner",
+                      "loop) should be < the relative tol of optim"))
     if (!is.null(sparse.calc)) {
         if (sparse.calc !=0 && sparse.calc !=1)
             stop("Invalid value for sparse.calc option")
         }
     list(eps=eps, toler.chol=toler.chol, iter.max=iter.max,
 	 inner.iter=inner.iter, sparse.calc=sparse.calc,
-         optpar=c(optpar, control=list(reltol=toler.opt))
-	 )
+         optpar=optpar)
     }
