1f137e3bd8dffb0e8405d3464e440b1029cf4c26
therneau (Terry Therneau) <therneau.terry@mayo.edu>
2010-07-09 17:42:38 +0000

    Replace one bit of printout with and "all.equal" construct instead.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/survival@11420 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/survival/tests/counting.R
+++ pkg/survival/tests/counting.R
@@ -38,6 +38,10 @@ aeq(resid(fit, type='score'), resid(fitb, type='score', collapse=test1b$id))
 aeq(resid(fit0, type='scho'), resid(fit0b, type='scho', collapse=test1b$id))
 aeq(resid(fit, type='scho'), resid(fitb, type='scho', collapse=test1b$id))
 
-summary(survfit(fit, list(x=0)))
-summary(survfit(fitb,list(x=0)))
+# The two survivals will have different censoring times
+#  nrisk, nevent, surv, and std should be the same
+temp1 <- survfit(fit, list(x=1), censor=FALSE)
+temp2 <- survfit(fitb, list(x=1), censor=FALSE)
+all.equal(unclass(temp1)[c(3,4,6,8)], unclass(temp2)[c(3,4,6,8)])
+
 
